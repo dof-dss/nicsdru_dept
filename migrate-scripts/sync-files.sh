2f293ff2e08cb351df8f9fd0b9735c2660d987a4
Johan Gant <johan@jnca-consulting.com>
2021-09-16 16:40:31 +0100

    Fix appservice reference

--- migrate-scripts/sync-files.sh
+++ migrate-scripts/sync-files.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 D7_PROJECT_ID=$1
 D7_PROJECT_ENV=$2
+APPSERVICE=deptinternet
 
-platform mount:download -p $D7_PROJECT_ID -e $D7_PROJECT_ENV -A dept -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --exclude="ctools*" --target /app/imports/files/ -y
+platform mount:download -p $D7_PROJECT_ID -e $D7_PROJECT_ENV -A $APPSERVICE -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --exclude="ctools*" --target /app/imports/files/ -y
