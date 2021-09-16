71f38214ebcabd7d368edcbaf9c337fde32e99ca
Johan Gant <johan@jnca-consulting.com>
2021-09-16 14:59:31 +0100

    Add script to import db and files at regular intervals

---
+++ migrate-scripts/sync-files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+D7_PROJECT_ID=$1
+D7_PROJECT_ENV=$2
+
+platform mount:download -p $D7_PROJECT_ID -e $D7_PROJECT_ENV -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --exclude="ctools*" --target /app/imports/files/ -y
