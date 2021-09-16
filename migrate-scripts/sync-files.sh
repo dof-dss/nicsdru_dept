0e8214f2d5fbf7ea542ea19adbf3c9078b4c7b76
Johan Gant <johan@jnca-consulting.com>
2021-09-16 16:29:58 +0100

    Conf tweak

--- migrate-scripts/sync-files.sh
+++ migrate-scripts/sync-files.sh
@@ -2,4 +2,4 @@
 D7_PROJECT_ID=$1
 D7_PROJECT_ENV=$2
 
-platform mount:download -p $D7_PROJECT_ID -e $D7_PROJECT_ENV -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --exclude="ctools*" --target /app/imports/files/ -y
+platform mount:download -p $D7_PROJECT_ID -e $D7_PROJECT_ENV -A dept -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --exclude="ctools*" --target /app/imports/files/ -y
