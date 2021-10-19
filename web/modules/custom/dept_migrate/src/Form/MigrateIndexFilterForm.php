47631cc2bc5b4eb781411b6616f41a04f447eebc
Johan Gant <johan@jnca-consulting.com>
2021-10-19 09:56:26 +0100

    Migration index UI to help track content between D7 and D9 (#4)
    * Initial service registry file and mapped class
    
    * Refined lookup functions in the service class
    
    * Early swing at tabular output on route defined in Controller
    
    * Refined mapping functions and UI
    
    * Fix callback reference
    
    * Extruding filter form into own class file, referenced from controller
    
    * Fix menu task link and adjust/remove sort options as they're misbehaving

---
+++ web/modules/custom/dept_migrate/src/Form/MigrateIndexFilterForm.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\dept_migrate\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MigrateIndexFilterForm extends FormBase {
+
+  /**
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected RequestStack $request;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+    $instance->request = $container->get('request_stack');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'migrate_index_filter_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Form filter.
+    $form['filter_type'] = [
+      '#title' => $this->t('Filter type'),
+      '#type' => 'select',
+      '#options' => [
+        'type' => 'Content type',
+        'nid' => 'Node ID',
+        'uuid' => 'UUID',
+        'title' => 'Title',
+      ],
+      '#default_value' => $this->request->getCurrentRequest()->query->get('filter_type'),
+    ];
+    $form['filter_value'] = [
+      '#title' => $this->t('Filter value'),
+      '#type' => 'textfield',
+      '#default_value' => $this->request->getCurrentRequest()->query->get('filter_value'),
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Filter'),
+      '#attributes' => ['class' => ['button--primary']],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_values = $form_state->getValues();
+
+    if (!empty($form_values['filter_type'])) {
+      $filter_value = $form_values['filter_value'] ?? '';
+
+      $form_state->setRedirect('dept_migrate.default', [], [
+        'query' => [
+          'filter_type' => $form_values['filter_type'],
+          'filter_value' => $filter_value,
+        ],
+      ]);
+    }
+  }
+
+}
