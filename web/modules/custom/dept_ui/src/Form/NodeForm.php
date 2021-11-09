6828e4b88fefbd18af7a2eeef37d2de129f04cf7
omahm <hello@onemanandhismouse.com>
2021-11-09 15:01:09 +0000

    List users groups on node add/edit form

--- web/modules/custom/dept_ui/src/Form/NodeForm.php
+++ web/modules/custom/dept_ui/src/Form/NodeForm.php
@@ -2,8 +2,16 @@
 
 namespace Drupal\dept_ui\Form;
 
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Drupal\group\GroupMembershipLoaderInterface;
 use Drupal\node\NodeForm as CoreNodeForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Form handler for the node add/edit forms.
@@ -11,10 +19,111 @@ use Drupal\node\NodeForm as CoreNodeForm;
 class NodeForm extends CoreNodeForm {
 
   /**
+   * The tempstore factory.
+   *
+   * @var \Drupal\Core\TempStore\PrivateTempStoreFactory
+   */
+  protected $tempStoreFactory;
+
+  /**
+   * The Current User object.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * The Group membership loader service.
+   *
+   * @var \Drupal\group\GroupMembershipLoader
+   */
+  protected $groupMembership;
+
+  /**
+   * Constructs a NodeForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository.
+   * @param \Drupal\Core\TempStore\PrivateTempStoreFactory $temp_store_factory
+   *   The factory for the temp store object.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The date formatter service.
+   */
+  public function __construct(
+    EntityRepositoryInterface $entity_repository,
+    PrivateTempStoreFactory $temp_store_factory,
+    EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL,
+    TimeInterface $time = NULL,
+    AccountInterface $current_user,
+    DateFormatterInterface $date_formatter,
+    GroupMemberShipLoaderInterface $group_membership
+  ) {
+    parent::__construct(
+      $entity_repository,
+      $temp_store_factory,
+      $entity_type_bundle_info,
+      $time,
+      $current_user,
+      $date_formatter
+    );
+
+    $this->groupMembership = $group_membership;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('tempstore.private'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('current_user'),
+      $container->get('date.formatter'),
+      $container->get('group.membership_loader')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
+
+    $user_memberships = $this->groupMembership->loadByUser();
+
+    foreach ($user_memberships as $membership) {
+      $group = $membership->getGroup();
+      $group_options[$group->id()] = $group->label();
+    }
+
+    $form['group_publish'] = [
+      '#title' => t('Groups'),
+      '#type' => 'details',
+      '#open' => TRUE,
+      '#group' => 'advanced',
+      '#weight' => -50,
+    ];
+
+    $form['group_publish']['groups'] = [
+      '#type' => 'checkboxes',
+      '#options' => $group_options,
+    ];
+
     return $form;
   }
 
