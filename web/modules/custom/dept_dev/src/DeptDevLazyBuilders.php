1370391b28fc3cbeeb13a62b2eac6ae67cc1822d
omahm <hello@onemanandhismouse.com>
2021-11-30 10:58:20 +0000

    Lazy builder to create Sites menu items

---
+++ web/modules/custom/dept_dev/src/DeptDevLazyBuilders.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\dept_dev;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Security\TrustedCallbackInterface;
+use Drupal\Core\Url;
+
+/**
+ * Dept Dev Lazy builders.
+ */
+class DeptDevLazyBuilders implements TrustedCallbackInterface {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Dept Dev LazyBuilders Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, RendererInterface $renderer) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks() {
+    return ['lazyLinks'];
+  }
+
+  /**
+   * #lazy_builder callback; builds departmental site links.
+   *
+   * @return array
+   *   A renderable array of site links.
+   */
+  public function lazyLinks() {
+    $domains = $this->entityTypeManager->getStorage('domain')->loadMultiple();
+    $links = [];
+
+    foreach ($domains as $domain) {
+      $links[$domain->id()] = [
+          'title' => $domain->label(),
+          'url' => Url::fromUri($domain->getPath())
+      ];
+    }
+
+    return [
+      '#theme' => 'links__toolbar_sites',
+      '#links' => $links,
+      '#attributes' => [
+        'class' => ['toolbar-menu'],
+      ],
+    ];
+  }
+
+}
