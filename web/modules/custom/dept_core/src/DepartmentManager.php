03646fa4c6e471cc95dc7df5b6e957cf783c31a4
omahm <hello@onemanandhismouse.com>
2021-12-01 20:56:32 +0000

    Method to get the current Department

--- web/modules/custom/dept_core/src/DepartmentManager.php
+++ web/modules/custom/dept_core/src/DepartmentManager.php
@@ -3,6 +3,7 @@
 namespace Drupal\dept_core;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\domain\DomainNegotiatorInterface;
 use Drupal\example\ExampleInterface;
 
 /**
@@ -13,7 +14,7 @@ class DepartmentManager {
   /**
    * The domain.negotiator service.
    *
-   * @var \Drupal\example\ExampleInterface
+   * @var \Drupal\domain\DomainNegotiatorInterface
    */
   protected $domainNegotiator;
 
@@ -27,15 +28,18 @@ class DepartmentManager {
   /**
    * Constructs a DepartmentManager object.
    *
-   * @param \Drupal\example\ExampleInterface $domain_negotiator
-   *   The domain.negotiator service.
+   * @param \Drupal\domain\DomainNegotiatorInterface $domain_negotiator
+   *   The Domain negotiator service.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    */
-  public function __construct(ExampleInterface $domain_negotiator, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(DomainNegotiatorInterface $domain_negotiator, EntityTypeManagerInterface $entity_type_manager) {
     $this->domainNegotiator = $domain_negotiator;
     $this->entityTypeManager = $entity_type_manager;
   }
 
-
+  public function getCurrentDepartment() {
+    $active_domain = $this->domainNegotiator->getActiveDomain();
+    return new Department($active_domain->id());
+  }
 }
