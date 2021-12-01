cd94c2fef79a99429930ef3b8e2e121b6534aa8f
omahm <hello@onemanandhismouse.com>
2021-12-01 13:02:47 +0000

    DEPT-55 ~ Block for displaying departmental info

--- web/modules/custom/dept_core/src/Plugin/Block/DepartmentalDetailsBlock.php
+++ web/modules/custom/dept_core/src/Plugin/Block/DepartmentalDetailsBlock.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\dept_core\Plugin\Block;
 
+use Drupal;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -11,28 +12,35 @@ use Drupal\Core\Form\FormStateInterface;
  * @Block(
  *   id = "dept_core_departmental_details",
  *   admin_label = @Translation("Departmental details"),
- *   category = @Translation("Custom")
+ *   category = @Translation("Departmental sites")
  * )
  */
 class DepartmentalDetailsBlock extends BlockBase {
 
-  /**
-   * {@inheritdoc}
-   */
   public function defaultConfiguration() {
-    return [
-      'foo' => $this->t('Hello world!'),
-    ];
+    return [];
   }
 
   /**
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
-    $form['foo'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Foo'),
-      '#default_value' => $this->configuration['foo'],
+
+    $options = [];
+    $group_fields = Drupal::service('entity_type.manager')->getStorage('field_storage_config')->loadByProperties([
+          'entity_type' => 'group'
+      ]
+    );
+
+    foreach ($group_fields as $field) {
+      $options[$field->id()] = $field->getLabel();
+    }
+
+    $form['display_field'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Field to display'),
+      '#options' => $options,
+      '#default_value' => $this->configuration['display_field'],
     ];
     return $form;
   }
@@ -41,17 +49,23 @@ class DepartmentalDetailsBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['foo'] = $form_state->getValue('foo');
+    $this->configuration['display_field'] = $form_state->getValue('display_field');
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+
+    ksm(\Drupal::service('domain.negotiator')->getActiveDomain(), \Drupal::service('group.group_route_context')->getGroupFromRoute());
+//    ksm(\Drupal::service('plugin.manager.domain_group_settings')->getAll()->get('domain_group_site_settings'));
+
     $build['content'] = [
-      '#markup' => $this->t('It works!'),
+      '#markup' =>  'foo',
     ];
     return $build;
   }
 
 }
+
+!YdufCeNr2xh4gGntyRxa
