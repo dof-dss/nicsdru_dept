998348478a2b92a3d6d6a723b098a41cde7f75a0
omahm <hello@onemanandhismouse.com>
2021-12-06 21:28:13 +0000

    DEPT-55 ~ Update block options to use department method names

--- web/modules/custom/dept_core/src/Plugin/Block/DepartmentalDetailsBlock.php
+++ web/modules/custom/dept_core/src/Plugin/Block/DepartmentalDetailsBlock.php
@@ -26,19 +26,17 @@ class DepartmentalDetailsBlock extends BlockBase {
    */
   public function blockForm($form, FormStateInterface $form_state) {
 
-    $options = [];
-    $group_fields = Drupal::service('entity_type.manager')->getStorage('field_storage_config')->loadByProperties([
-          'entity_type' => 'group'
-      ]
-    );
-
-    foreach ($group_fields as $field) {
-      $options[$field->id()] = $field->getLabel();
-    }
+    $options = [
+      'accessToInformation' => 'Access to information',
+      'contactInformation' => 'Contact Information',
+      'managementAndStructure' => 'Management and structure',
+      'socialMediaLinks' => 'Social media links',
+      'location' => 'Location',
+    ];
 
     $form['display_field'] = [
       '#type' => 'select',
-      '#title' => $this->t('Field to display'),
+      '#title' => $this->t('Information to display'),
       '#options' => $options,
       '#default_value' => $this->configuration['display_field'],
     ];
@@ -57,15 +55,10 @@ class DepartmentalDetailsBlock extends BlockBase {
    */
   public function build() {
 
-    ksm(\Drupal::service('domain.negotiator')->getActiveDomain(), \Drupal::service('group.group_route_context')->getGroupFromRoute());
-//    ksm(\Drupal::service('plugin.manager.domain_group_settings')->getAll()->get('domain_group_site_settings'));
-
     $build['content'] = [
-      '#markup' =>  'foo',
+      '#markup' =>  'content',
     ];
     return $build;
   }
 
 }
-
-!YdufCeNr2xh4gGntyRxa
