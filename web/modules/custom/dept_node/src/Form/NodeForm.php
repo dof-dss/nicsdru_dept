714bb41aab0d7e3fc3e93b64f4b10673c6f7b875
Steve Cousins <omahm@users.noreply.github.com>
2021-12-13 18:46:11 +0000

    Fix for when editing a node that isn't part of a group

--- web/modules/custom/dept_node/src/Form/NodeForm.php
+++ web/modules/custom/dept_node/src/Form/NodeForm.php
@@ -107,6 +107,11 @@ class NodeForm extends CoreNodeForm {
    */
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
+    
+    // Return if the bundle isn't present as a Group content plugin.
+    if (!method_exists($this->entity, 'groupBundle')) {
+      return $form;
+    }
 
     $content_groups = [];
     $plugin_id = $this->entity->groupBundle();
