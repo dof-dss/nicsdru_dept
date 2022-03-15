4d972241e83ce9a7e452d0783316fd01d0ed0ac5
Johan Gant <johan@jnca-consulting.com>
2022-03-15 16:35:40 +0000

    Update migrate batch script

--- migrate-scripts/migrate.sh
+++ migrate-scripts/migrate.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export DRUSH=/app/vendor/bin/drush
 export MIGRATIONS="d7_taxonomy_term_chart_type d7_taxonomy_term_global_topics d7_taxonomy_term_indicators \
-        d7_taxonomy_term_outcomes users d7_file d7_file_media_image d7_file_media_video \
-          group_media_image group_media_video node_easychart node_news"
+        d7_taxonomy_term_outcomes users d7_file d7_file_private d7_file_media_image d7_file_media_video \
+          node_easychart node_news node_publication"
 
 if [ -z ${PLATFORM_BRANCH} ] && [ -z ${LANDO} ];
 then
@@ -36,15 +36,15 @@ then
 
   echo "Migrating D7 files and media entities"
   $DRUSH migrate:import d7_file --force
+  $DRUSH migrate:import d7_file_private --force
+
   for type in image video
   do
     echo "Migrating D7 ${type} to corresponding media entities"
     $DRUSH migrate:import d7_file_media_$type --force
-    echo "... associate ${type} media entities with Group entities"
-    $DRUSH migrate:import group_media_$type --force
   done
 
-  for type in easychart news
+  for type in easychart news publication
   do
     echo "Migrate D7 ${type} nodes"
     $DRUSH migrate:import node_$type --force
