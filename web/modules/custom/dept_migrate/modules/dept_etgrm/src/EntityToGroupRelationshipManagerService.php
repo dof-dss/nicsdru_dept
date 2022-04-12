b2c278380668e27bad0a76ad6bdb257b58074000
omahm <hello@onemanandhismouse.com>
2022-04-12 18:31:52 +0100

    DEPT-114 ~ Basic remove all method

--- web/modules/custom/dept_migrate/modules/dept_etgrm/src/EntityToGroupRelationshipManagerService.php
+++ web/modules/custom/dept_migrate/modules/dept_etgrm/src/EntityToGroupRelationshipManagerService.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\dept_etgrm;
 
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
@@ -10,11 +11,23 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 class EntityToGroupRelationshipManagerService {
 
   /**
+   * The type of database operation to perform.
+   */
+  protected $action;
+
+  /**
    * The entity type manager.
    *
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $dbConn;
 
 
   /**
@@ -22,9 +35,33 @@ class EntityToGroupRelationshipManagerService {
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The database connection.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, Connection $connection) {
     $this->entityTypeManager = $entity_type_manager;
+    $this->dbConn = $connection;
   }
+
+  public function create() {
+    $this->action = 'create';
+    return $this;
+  }
+
+  public function remove() {
+    $this->action = 'remove';
+    return $this;
+  }
+
+  public function all() {
+    if ($this->action === 'remove') {
+      $this->dbConn->truncate(['group_content'])->execute();
+      $this->dbConn->truncate(['group_content_field_data'])->execute();
+    }
+    else {
+
+    }
+  }
+
 
 }
