bcea542b763e01e73e3b656d1aec2cad38bf6d13
omahm <hello@onemanandhismouse.com>
2022-04-12 15:06:00 +0100

    DEPT-105 ~ Fetch bundles associated with group and iterate for each department

--- web/modules/custom/dept_migrate/modules/dept_migrate_dashboard/src/Controller/MigrationDashboardController.php
+++ web/modules/custom/dept_migrate/modules/dept_migrate_dashboard/src/Controller/MigrationDashboardController.php
@@ -3,6 +3,7 @@
 namespace Drupal\dept_migrate_dashboard\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\dept_core\DepartmentManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -20,13 +21,24 @@ class MigrationDashboardController extends ControllerBase {
   protected $departmentManager;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+
+  /**
    * Migrations dashboard constructor.
    *
    * @param \Drupal\dept_core\DepartmentManager $department_manager
    *   The DepartmentManager service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    */
-  public function __construct(DepartmentManager $department_manager) {
+  public function __construct(DepartmentManager $department_manager, EntityTypeManagerInterface $entity_type_manager) {
     $this->departmentManager = $department_manager;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -39,7 +51,8 @@ class MigrationDashboardController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('department.manager')
+      $container->get('department.manager'),
+      $container->get('entity_type.manager')
     );
   }
 
@@ -50,17 +63,36 @@ class MigrationDashboardController extends ControllerBase {
   public function build() {
 
     $departments = $this->departmentManager->getAllDepartments();
+    $group_type = $this->entityTypeManager->getStorage('group_type')->load('department_site');
+    $node_bundles = [];
+    $table_header = ['Bundle', 'Drupal 9', 'Drupal 7'];
+
+    foreach ($group_type->getInstalledContentPlugins() as $plugin) {
+      if ($plugin->getEntityTypeId() === 'node') {
+        $node_bundles[] = $plugin->getEntityBundle();
+      }
+    }
+
 
     /** @var \Drupal\dept_core\Department $department **/
     foreach ($departments as $department) {
+      $rows = [];
 
-      $build['department_' . $department->id()] = [
-        'fieldset' => [
-          '#type' => 'fieldset',
+      $build['department_' . $department->id()]['wrapper'] = [
+          '#type' => 'details',
           '#title' => $department->name(),
-        ],
+          '#open' => TRUE,
       ];
 
+      foreach ($node_bundles as $bundle) {
+        $rows[] = [$bundle, 10, 10];
+      }
+
+      $build['department_' . $department->id()]['wrapper']['nodes'] = [
+        '#type' => 'table',
+        '#header' => $table_header,
+        '#rows' => $rows,
+      ];
     }
 
     return $build;
