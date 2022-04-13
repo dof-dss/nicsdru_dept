d60cbac61804b1eb7dc4088c90e5036cfa0e0d9e
omahm <hello@onemanandhismouse.com>
2022-04-13 11:15:26 +0100

    DEPT-114 ~ Create all relationships

--- web/modules/custom/dept_migrate/modules/dept_etgrm/src/EntityToGroupRelationshipManagerService.php
+++ web/modules/custom/dept_migrate/modules/dept_etgrm/src/EntityToGroupRelationshipManagerService.php
@@ -4,6 +4,9 @@ namespace Drupal\dept_etgrm;
 
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\dept_core\DepartmentManager;
+use Drupal\group\Entity\Group;
+use Drupal\group\Entity\GroupContent;
 
 /**
  * EntityToGroupRelationshipManagerService service.
@@ -73,7 +76,55 @@ class EntityToGroupRelationshipManagerService {
       $this->dbConn->truncate('group_content_field_data')->execute();
     }
     else {
+      // Get a list of content types used by the departmental group
+      // Iterate each group, lookup the mapping table, for each row
+      // create a relationship based on the data from sourceid3 and destid1
+      $departments = $this->entityTypeManager->getStorage('group_type')->load('department_site');
 
+      foreach ($departments->getInstalledContentPlugins() as $plugin) {
+        if ($plugin->getEntityTypeId() === 'node') {
+          $bundle = $plugin->getEntityBundle();
+          $counter = 0;
+
+          $migration_table = 'migrate_map_node_' . $bundle;
+
+          if ($this->dbConn->schema()->tableExists($migration_table)) {
+            $query = $this->dbConn->select($migration_table, 'mt');
+            $query->addField('mt', 'sourceid3', 'domains');
+            $query->addField('mt', 'destid1', 'nid');
+            $result = $query->execute();
+
+            foreach ($result->fetchAll() as $row) {
+
+              $node = $this->entityTypeManager->getStorage('node')->load($row->nid);
+
+              if (empty($node)) {
+                continue;
+              }
+              $relationships = GroupContent::loadByEntity($node);
+              $groups = [];
+
+              foreach ($relationships as $relation) {
+                $groups[] = $relation->getGroup()->id();
+              }
+
+              foreach (explode('-', $row->domains) as $domain) {
+                if (!in_array($domain, $groups)) {
+                  $group = Group::load($domain);
+
+                  if (empty($group)) {
+                    continue;
+                  }
+                  $group->addContent($node, 'group_node:' . $node->bundle());
+                  $counter++;
+                }
+              }
+            }
+          }
+        }
+
+        return $counter;
+      }
     }
   }
 
