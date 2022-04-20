4af9b12be8b18a472ec24f71bc6645646fbb48c6
omahm <hello@onemanandhismouse.com>
2022-04-20 14:32:04 +0100

    DEPT-114 ~ Initial batch api calls to create group relationships

--- web/modules/custom/dept_migrate/modules/dept_etgrm/src/Form/CreateAllRelationshipsForm.php
+++ web/modules/custom/dept_migrate/modules/dept_etgrm/src/Form/CreateAllRelationshipsForm.php
@@ -24,9 +24,26 @@ class CreateAllRelationshipsForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    $departments =  \Drupal::entityTypeManager()->getStorage('group_type')->load('department_site');
+    $etgrm = \Drupal::service('etgrm.manager');
+    $group_bundles = [];
+
+    foreach ($departments->getInstalledContentPlugins() as $plugin) {
+      if ($plugin->getEntityTypeId() === 'node') {
+        $group_bundles[$plugin->getEntityBundle()] = $plugin->getEntityBundle();
+      }
+    }
+
+    $form['bundle'] = [
+      '#type' => 'select',
+      '#options' => $group_bundles,
+      '#title' => $this->t("Bundle"),
+    ];
+
+
     $form['actions']['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Create all relationships'),
+      '#value' => $this->t('Create group relationships'),
     ];
 
     return $form;
@@ -36,83 +53,36 @@ class CreateAllRelationshipsForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $bundle = $form_state->getValue('bundle');
 
-
-    $departments =  \Drupal::entityTypeManager()->getStorage('group_type')->load('department_site');
     $etgrm_service = \Drupal::service('etgrm.manager');
 
-    foreach ($departments->getInstalledContentPlugins() as $plugin) {
-      if ($plugin->getEntityTypeId() === 'node') {
-        $bundles[] = $plugin->getEntityBundle();
-      }
-    }
+    $migration_table = 'migrate_map_node_' . $bundle;
 
+    $query = \Drupal::database()->select($migration_table, 'mt');
+    $query->addField('mt', 'sourceid3', 'domains');
+    $query->addField('mt', 'destid1', 'nid');
+    $result = $query->execute();
+
+    $rows = $result->fetchAllAssoc('nid');
 
     $batch_builder = new BatchBuilder();
 
     $batch_builder
-      ->setTitle($this->t('Creating node batches for @num bundles', ['@num' => $bundles,]))
+      ->setTitle($this->t('Creating group relationships for @bundle', ['@bundle' => $bundle,]))
       ->setFile(\Drupal::service('extension.list.module')->getPath('dept_etgrm') . '/etgrm.batch.inc')
-      ->setFinishCallback('buildRelationshipBatchFinished')
+      ->setFinishCallback('buildGroupRelationshipsFinished')
       ->setErrorMessage(t('Batch has encountered an error'));
 
-    foreach ($bundles as $bundle) {
-      $batch_builder->addOperation('buildRelationshipBatch', [
+    foreach ($rows as $row) {
+      $batch_builder->addOperation('buildGroupRelationships', [
+        $row,
         $bundle,
         $etgrm_service,
       ]);
     }
 
     batch_set($batch_builder->toArray());
-
-//    $etgrm = new EntityToGroupRelationshipManagerService();
-//    $etgrm->create()->all();
-//
-//    foreach ($etgrm->batch as $batch) {
-//
-//    }
-//
-//
-//    $bundle = 'article';
-//
-//    $migration_table = 'migrate_map_node_' . $bundle;
-//
-//    $query = \Drupal::database()->select($migration_table, 'mt');
-//    $query->addField('mt', 'sourceid3', 'domains');
-//    $query->addField('mt', 'destid1', 'nid');
-//    $result = $query->execute();
-//
-//    $rows = $result->fetchAllAssoc('nid');
-//
-//    $etgrm_service = \Drupal::service('etgrm.manager');
-//
-//    $batch_builder = new BatchBuilder();
-//    $total_nodes = count($rows);
-//    $batchId = 1;
-//
-//    $batch_builder
-//      ->setTitle($this->t('Processing group relationships for @num nodes', ['@num' => $total_nodes,]))
-//      ->setFile(\Drupal::service('extension.list.module')->getPath('dept_etgrm') . '/etgrm.batch.inc')
-//      ->setFinishCallback('processGroupNodesFinished')
-//      ->setErrorMessage(t('Batch has encountered an error'));
-//
-//    foreach ($rows as $row) {
-//      $group_ids = [];
-//      foreach (explode('-', $row->domains) as $domain) {
-//        $group_ids[] = EntityToGroupRelationshipManagerService::domainIDtoGroupId($domain);
-//      }
-//
-//      $batch_builder->addOperation('processGroupNodes', [
-//        $batchId,
-//        $row->nid,
-//        $group_ids,
-//        $etgrm_service,
-//      ]);
-//      $batchId++;
-//      $total_nodes++;
-//    }
-//
-//    batch_set($batch_builder->toArray());
   }
 
 }
