4463da1331604244f9e0ad67adf435a078ce153b
omahm <hello@onemanandhismouse.com>
2022-05-02 19:11:53 +0100

    DEPT-97 ~ View and edit domain id mappings

--- web/modules/custom/dept_dev/src/Form/SettingsForm.php
+++ web/modules/custom/dept_dev/src/Form/SettingsForm.php
@@ -36,6 +36,22 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $this->config('dept_dev.settings')->get('node_source_link'),
     ];
 
+    $form['node_source_domains'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Domain ID to URL mappings'),
+      '#open' => TRUE,
+    ];
+
+    $domains = $this->config('dept_dev.settings')->get('node_source_domains');
+
+    foreach ($domains as $key => $domain) {
+      $form['node_source_domains']['node_source_domain_' . $key] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('ID: @id', ['@id' => $key] ),
+        '#default_value' => $domain,
+      ];
+    }
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -43,10 +59,21 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_vals = $form_state->getValues();
+
+    foreach ($form_vals as $key => $val) {
+      if (substr( $key, 0, 19) === "node_source_domain_") {
+        $domains[substr( $key, 19)] = $val;
+      }
+    }
+
     $config = $this->config('dept_dev.settings');
-    $config->set('node_source_link', $form_state->getValue('node_source_link'));
+    $config->set('node_source_domains', $domains);
+    $config->set('node_source_link', $form_vals['node_source_link']);
     $config->save();
+
     parent::submitForm($form, $form_state);
   }
 
 }
+
