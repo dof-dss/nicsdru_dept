e99824a9214fdd982a85fa3c748f02e895bd7f9f
omahm <hello@onemanandhismouse.com>
2022-05-18 15:25:46 +0100

    DEPT-65 ~ Replace relative link to absolute for the node

--- web/modules/custom/dept_core/modules/dept_rel_to_abs_urls/src/Plugin/Filter/relToAbsUrlsFilter.php
+++ web/modules/custom/dept_core/modules/dept_rel_to_abs_urls/src/Plugin/Filter/relToAbsUrlsFilter.php
@@ -15,14 +15,36 @@ use Drupal\filter\Plugin\FilterBase;
  */
 class relToAbsUrlsFilter extends FilterBase {
 
+  // TODO: Add an admin option to restrict this by domain.
+
   public function process($text, $langcode) {
     $result = new FilterProcessResult($text);
+
 
     $updated_text = preg_replace_callback(
       '/data-entity-uuid="(.+)" href="(\/\S+)"/m',
       static function ($matches) {
-        // TODO: Replace with node group domain.
-        return 'href="' . strtoupper($matches[2]) . '"';
+        // TODO: Inject
+        $node  = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['uuid' => $matches[1]]);
+        $node = reset($node);
+        // TODO: Check node object has getGroups method or is of a group content type plugin.
+        $groups = $node->getGroups();
+
+        if (!empty($groups)) {
+          $group_id = array_key_first($groups);
+
+          // TODO: Inject service.
+          /* @var $dept_man \Drupal\dept_core\DepartmentManager */
+          $dept_man = \Drupal::service('department.manager');
+
+          /* @var $dept \Drupal\dept_core\Department*/
+          $dept = $dept_man->getDepartment('group_' . $group_id);
+          $hostname = $dept->hostname();
+
+          return 'href="https://' . $hostname . $matches[2] . '"';
+        }
+
+
       },
       $result
     );
