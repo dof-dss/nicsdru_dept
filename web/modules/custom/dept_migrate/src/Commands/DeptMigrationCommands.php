3de7f88ff16a1b38fc519445113406446f6d099a
omahm <hello@onemanandhismouse.com>
2022-05-20 11:30:22 +0100

    DEPT-125 ~ Fetch rows of text based fields that contain node/XXXX content

--- web/modules/custom/dept_migrate/src/Commands/DeptMigrationCommands.php
+++ web/modules/custom/dept_migrate/src/Commands/DeptMigrationCommands.php
@@ -3,6 +3,7 @@
 namespace Drupal\dept_migrate\Commands;
 
 use Drupal\Core\Database\Database;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drush\Commands\DrushCommands;
 
@@ -19,12 +20,30 @@ class DeptMigrationCommands extends DrushCommands {
    * @command dept:updatelinks
    * @aliases dept:uplnks
    */
-  public function all() {
+  public function updateInternalLinks() {
+
+    $tables = [
+      'body',
+      'field_additional_info',
+      'field_summary',
+    ];
 
     $dbConn = Database::getConnection('default', 'default');
 
     $this->io()->title("Updating all internal links");
 
+    foreach ($tables as $table) {
+
+      $query = $dbConn->select('node__' . $table, 't');
+      $query->addField('t', 'entity_id', 'id');
+      $query->addField('t', $table . '_value', 'value');
+      $query->condition($table . '_value', 'node\/[0-9]+', 'REGEXP');
+      $results = $query->execute()->fetchAll();
+
+      foreach ($results as $record) {
+        $this->io()->writeln($record->id);
+      }
+    }
 
     $this->io()->success("Finished");
   }
