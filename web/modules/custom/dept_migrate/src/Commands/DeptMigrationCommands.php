cfcf0da6dead90c61f4da1172bb5757727269768
omahm <hello@onemanandhismouse.com>
2022-05-23 10:10:48 +0100

    DEPT-125 ~ Inject services

--- web/modules/custom/dept_migrate/src/Commands/DeptMigrationCommands.php
+++ web/modules/custom/dept_migrate/src/Commands/DeptMigrationCommands.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\dept_migrate\Commands;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Database\Database;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\dept_migrate\MigrateUuidLookupManager;
 use Drush\Commands\DrushCommands;
 
 /**
@@ -13,6 +15,29 @@ use Drush\Commands\DrushCommands;
 class DeptMigrationCommands extends DrushCommands {
 
   use StringTranslationTrait;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $dbConn;
+
+  /**
+   * Migration Lookup Manager.
+   *
+   * @var \Drupal\dept_migrate\MigrateUuidLookupManager
+   */
+  protected $lookupManager;
+
+  /**
+   * MyModuleCommands constructor.
+   */
+  public function __construct(Connection $database, MigrateUuidLookupManager $lookup_manager ) {
+    parent::__construct();
+    $this->dbConn = $database;
+    $this->lookupManager = $lookup_manager;
+  }
 
   /**
    * Updates all internal /node/XXXX links from their D7 to the D9 nid.
@@ -28,31 +53,27 @@ class DeptMigrationCommands extends DrushCommands {
       'field_summary',
     ];
 
-    $dbConn = Database::getConnection('default', 'default');
-    $lookupMan = \Drupal::service('dept_migrate.migrate_uuid_lookup_manager');
-
     foreach ($fields as $field) {
       $table = 'node__' . $field;
-      $query = $dbConn->select($table, 't');
+      $query = $this->dbConn->select($table, 't');
       $query->addField('t', 'entity_id', 'nid');
       $query->addField('t', $field . '_value', 'value');
       $query->condition($field . '_value', 'node\/[0-9]+', 'REGEXP');
 
       $results = $query->execute()->fetchAll();
 
-
       foreach ($results as $result) {
         $updated_value = preg_replace_callback(
           '/(<a href="\/node\/)(\d+)/m',
-          function ($matches) use ($lookupMan, $dbConn) {
-            $d9_lookup = $lookupMan->lookupBySourceNodeId([$matches[2]]);
+          function ($matches) {
+            $d9_lookup =  $this->lookupManager->lookupBySourceNodeId([$matches[2]]);
 
             if (!empty($d9_lookup)) {
               $node_data = current($d9_lookup);
 
               if (!empty($node_data['nid'])) {
                 $d9_nid = $node_data['nid'];
-                $d9_uuid = $dbConn->query('SELECT uuid FROM {node} WHERE nid = :nid', [':nid' => $d9_nid])->fetchField();
+                $d9_uuid = $this->dbConn->query('SELECT uuid FROM {node} WHERE nid = :nid', [':nid' => $d9_nid])->fetchField();
 
                 return '<a data-entity-substitution="canonical" data-entity-type="node" data-entity-uuid="' . $d9_uuid . '" href="/node/' . $node_data['nid'];
               }
@@ -61,7 +82,7 @@ class DeptMigrationCommands extends DrushCommands {
           $result->value
         );
 
-        $dbConn->update($table)
+        $this->dbConn->update($table)
           ->fields([$field . '_value' => $updated_value])
           ->condition('entity_id', $result->nid, '=')
           ->execute();
