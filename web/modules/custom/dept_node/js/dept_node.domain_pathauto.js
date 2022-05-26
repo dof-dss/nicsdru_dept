ea7e90d3b3c38ab1fdf5d75dbeb1f18136a3de4f
omahm <hello@onemanandhismouse.com>
2022-05-26 09:43:48 +0100

    DEPT-130 ~ Update Domain Pathauto options on page load

--- web/modules/custom/dept_node/js/dept_node.domain_pathauto.js
+++ web/modules/custom/dept_node/js/dept_node.domain_pathauto.js
@@ -6,14 +6,25 @@
 (function ($, Drupal) {
   Drupal.behaviors.deptNodeDomainPathauto = {
     attach: function (context, settings) {
-      // Event handler for publish to department checkboxes.
-      $( '#edit-groups input[type="checkbox"]', context ).change(function(e) {
-        if ($(this).prop('checked')) {
-          $('#edit-path-0-domain-path-group-' + $(this).val() + '-pathauto').prop('checked', true);
+
+      // Update Domain Pathauto generate URL option depending on Publish to options.
+      function updateDomainPathauto(element) {
+        if ($(element).prop('checked')) {
+          $('#edit-path-0-domain-path-group-' + $(element).val() + '-pathauto').prop('checked', true);
         } else {
-          $('#edit-path-0-domain-path-group-' + $(this).val() + '-pathauto').prop('checked', false);
+          $('#edit-path-0-domain-path-group-' + $(element).val() + '-pathauto').prop('checked', false);
         }
+      }
+
+      // Update Pathauto on page load.
+      $('#edit-groups input[type="checkbox"]', context).each(function () {
+        updateDomainPathauto($(this));
       });
-    }
+
+      // Event handler for publish to department checkboxes.
+      $('#edit-groups input[type="checkbox"]', context).change(function() {
+        updateDomainPathauto($(this));
+      });
+    },
   };
 })(jQuery, Drupal);
