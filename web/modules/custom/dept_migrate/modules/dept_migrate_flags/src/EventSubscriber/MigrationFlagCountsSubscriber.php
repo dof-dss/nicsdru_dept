13644b78468b242b57d103658295889b186c09f1
OMAHM <hello@onemanandhismouse.com>
2022-06-09 15:51:36 +0100

    D8NID-110 ~ Convert D7 nids to their D9 equivalent. Set count to 0 as the flagging migration will increment it

--- web/modules/custom/dept_migrate/modules/dept_migrate_flags/src/EventSubscriber/MigrationFlagCountsSubscriber.php
+++ web/modules/custom/dept_migrate/modules/dept_migrate_flags/src/EventSubscriber/MigrationFlagCountsSubscriber.php
@@ -3,6 +3,7 @@
 namespace Drupal\dept_migrate_flags\EventSubscriber;
 
 use Drupal\Core\Database\Connection;
+use Drupal\dept_migrate\MigrateUuidLookupManager;
 use Drupal\migrate\Event\MigrateEvents;
 use Drupal\migrate\Event\MigrateImportEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -27,16 +28,26 @@ class MigrationFlagCountsSubscriber implements EventSubscriberInterface {
   protected $d7dbconn;
 
   /**
+   * Departmental Migration lookup manager.
+   *
+   * @var \Drupal\dept_migrate\MigrateUuidLookupManager
+   */
+  protected $lookupManager;
+
+  /**
    * Constructs event subscriber.
    *
    * @param \Drupal\Core\Database\Connection $connection
    *   Database connection.
    * @param \Drupal\Core\Database\Connection $d7_connection
    *   D7 database connection.
+   * @param \Drupal\dept_migrate\MigrateUuidLookupManager $lookup_manager
+   *   The lookup manager.
    */
-  public function __construct( Connection $connection, Connection $d7_connection) {
+  public function __construct( Connection $connection, Connection $d7_connection, MigrateUuidLookupManager $lookup_manager) {
     $this->dbconn = $connection;
     $this->d7dbconn = $d7_connection;
+    $this->lookupManager = $lookup_manager;
   }
 
   /**
@@ -62,7 +73,10 @@ class MigrationFlagCountsSubscriber implements EventSubscriberInterface {
     $results = $this->d7dbconn->query("SELECT CASE WHEN fid = 4 THEN 'hide_listing' WHEN fid = 5 THEN 'hide_on_topic_subtopic_pages' WHEN fid = 6 THEN 'display_on_rss_feeds' END AS flag_id, entity_id, last_updated FROM {flag_counts} WHERE fid IN (4,5,6)");
 
       foreach ($results as $result) {
-        $this->dbconn->query("INSERT IGNORE INTO {flag_counts} (flag_id, entity_type, entity_id, count, last_updated) VALUES ('$result->flag_id', 'node', '$result->entity_id', '1', '$result->last_updated')")->execute();
+        $nids = $this->lookupManager->lookupBySourceNodeId([$result->entity_id]);
+
+        $d9_nid = $nids[$result->entity_id]['nid'];
+        $this->dbconn->query("INSERT IGNORE INTO {flag_counts} (flag_id, entity_type, entity_id, count, last_updated) VALUES ('$result->flag_id', 'node', '$d9_nid', '0', '$result->last_updated')")->execute();
       }
     }
 //  }
