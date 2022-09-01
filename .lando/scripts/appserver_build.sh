cb7c2f8b44405bcfe5f33a0c1e677a3e80e147c7
Johan Gant <johan@jnca-consulting.com>
2022-09-01 16:43:24 +0100

    Drop chromedriver/yarn/nightwatch from local dev tools

--- .lando/scripts/appserver_build.sh
+++ .lando/scripts/appserver_build.sh
@@ -5,10 +5,11 @@ DRUPAL_ROOT=/app/web
 DRUPAL_SETTINGS_FILE=$DRUPAL_ROOT/sites/default/settings.php
 DRUPAL_SERVICES_FILE=$DRUPAL_ROOT/sites/default/services.yml
 DRUPAL_CUSTOM_CODE=$DRUPAL_ROOT/modules/custom
+DRUPAL_CUSTOM_THEME=$DRUPAL_ROOT/themes/custom/nicsdru_dept_theme
 
 # Semaphore files to control whether we need to trigger an install
 # of supporting software or config files.
-NODE_YARN_INSTALLED=/etc/NODE_YARN_INSTALLED
+NODE_INSTALLED=/etc/NODE_INSTALLED
 
 # Create export directories for config and data.
 if [ ! -d "/app/.lando/exports" ]; then
@@ -53,40 +54,30 @@ if [ ! -f "${DRUPAL_ROOT}/core/phpunit.xml" ]; then
 fi
 
 # Add yarn/nodejs packages to allow functional testing on this service.
-if [ ! -f "${NODE_YARN_INSTALLED}" ]; then
-  # Update packages and add gnupg and https for apt to fetch yarn packages.
+if [ ! -f "${NODE_INSTALLED}" ]; then
   apt update
-  apt install -y gnupg apt-transport-https
-  # Add yarn deb repo.
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-  apt update
-  apt install -y yarn
-  # Add and fetch up to date nodejs to allow yarn to run correctly.
+  # Add and fetch node 14 plus related OS packages to support it.
   curl -sL https://deb.nodesource.com/setup_14.x | bash -
-  apt install -y nodejs
-
-  # Copy Drupal .env.example file, inject Lando vars and set in place for use by Nightwatch conf.
-  cat $DRUPAL_ROOT/core/.env.example | sed -e "s|\(^DRUPAL_TEST_BASE_URL\)\(.\+\)|\1=http:\/\/${LANDO_APP_NAME}.${LANDO_DOMAIN}|g" > $DRUPAL_ROOT/core/.env
-  # Alter a few more variables.
-  sed -i -e "s|\(#\)\(DRUPAL_NIGHTWATCH_SEARCH_DIRECTORY\)=|\2=../|g" $DRUPAL_ROOT/core/.env
-  sed -i -e "s|sqlite:\/\/localhost\/sites\/default\/files/db.sqlite|${DB_DRIVER}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}|g" $DRUPAL_ROOT/core/.env
-  sed -i -e "s|\(^DRUPAL_TEST_WEBDRIVER_HOSTNAME\)=localhost|\1=chromedriver|g" $DRUPAL_ROOT/core/.env
-  sed -i -e "s|^DRUPAL_TEST_CHROMEDRIVER_AUTOSTART=true|DRUPAL_TEST_CHROMEDRIVER_AUTOSTART=false|g" $DRUPAL_ROOT/core/.env
-  sed -i -e "s|\(#\)\(DRUPAL_TEST_WEBDRIVER_CHROME_ARGS\)=|\2=\"--disable-gpu --headless --no-sandbox\"|g" $DRUPAL_ROOT/core/.env
-  sed -i -e "s|\(^DRUPAL_NIGHTWATCH_OUTPUT\)=reports/nightwatch|\1=/app/.lando/exports/nightwatch-reports|g" $DRUPAL_ROOT/core/.env
+  apt install -y nodejs gcc g++ make
 
   # Fetch and install node packages if they're not already present.
   if [ ! -d "${DRUPAL_ROOT}/core/node_modules" ]; then
-    cd $DRUPAL_ROOT/core && yarn install
-  fi
-
-  # Install any known extra npm packges for, eg: migrations.
-    if [ ! -d "${DRUPAL_CUSTOM_CODE}/node_modules" ]; then
-    cd $DRUPAL_CUSTOM_CODE
+    cd $DRUPAL_ROOT/core
     npm install
   fi
 
-  touch $NODE_YARN_INSTALLED
+  # Install any known extra npm packges.
+#  if [ ! -d "${DRUPAL_CUSTOM_CODE}/node_modules" ]; then
+#    cd $DRUPAL_CUSTOM_CODE
+#    npm install
+#  fi
+
+  # Install any known extra npm packges.
+  if [ ! -d "${DRUPAL_CUSTOM_THEME}/node_modules" ]; then
+    cd $DRUPAL_CUSTOM_THEME
+    npm install
+  fi
+
+  touch $NODE_INSTALLED
 
 fi
