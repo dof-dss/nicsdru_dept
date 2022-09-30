01a845aa001512e653483c7ce75e564d1d6dcd72
Johan Gant <johan@jnca-consulting.com>
2022-09-30 15:43:59 +0100

    Introduce service to tweak how certain request paths behave from group to group

---
+++ web/modules/custom/dept_core/src/EventSubscriber/DepartmentPathSubscriber.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\dept_core\EventSubscriber;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\dept_core\Department;
+use Drupal\dept_core\DepartmentManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * This subscriber class allows us to shut off access to, or redirect,
+ * requests which on one group might be unwanted on another.
+ *
+ * Eg: northernireland.gov.uk/news (should be /press-releases)
+ * or daera-ni.gov.uk/press-releases (should be /news)
+ */
+class DepartmentPathSubscriber implements EventSubscriberInterface {
+
+  /**
+   * @var \Drupal\dept_core\DepartmentManager
+   */
+  protected $departmentManager;
+
+  /**
+   * @param \Drupal\dept_core\DepartmentManager $department_manager
+   *   Department manager service object.
+   */
+  public function __construct(DepartmentManager $department_manager) {
+    $this->departmentManager = $department_manager;
+  }
+
+  /**
+   * @param \Symfony\Component\HttpKernel\Event\ResponseEvent $event
+   *   The response event object.
+   */
+  public function onKernelResponseCheckGroupPath(ResponseEvent $event): void {
+    $request = clone $event->getRequest();
+    $path = $request->getPathInfo();
+
+    // What's the detected department/group?
+    /** @var \Drupal\dept_core\Department $active_dept */
+    $active_dept = $this->departmentManager->getCurrentDepartment();
+    if (!$active_dept instanceof Department) {
+      return;
+    }
+
+    /** @var \Drupal\Core\Cache\CacheableResponse $response */
+    $response = $event->getResponse();
+
+    if ($active_dept->name() === 'NIGov') {
+      // NIGOV would use /press-releases instead.
+      if ($path === '/news') {
+        $response->setStatusCode(404);
+      }
+    }
+    else {
+      // Non-NIGOV sites would use /news so return 404 for this path.
+      if ($path === '/press-releases') {
+        $response->setStatusCode(404);
+      }
+    }
+
+    // Always add a url.site cache context for anything we've touched.
+    $touched_response = $response->getStatusCode() != 200;
+    if ($touched_response) {
+      $cache_options = new CacheableMetadata();
+      $cache_options->setCacheContexts(['url.site']);
+      $response->addCacheableDependency($cache_options);
+      $event->setResponse($response);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    $events[KernelEvents::RESPONSE][] = ['onKernelResponseCheckGroupPath'];
+    return $events;
+  }
+
+}
