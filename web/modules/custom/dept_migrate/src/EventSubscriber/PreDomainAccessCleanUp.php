587dabe1739ea45e9a5c4dbbb402607714f9e360
OMAHM <hello@onemanandhismouse.com>
2022-10-31 15:19:19 +0000

    DEPT-230 ~ Only execute when running node migrations

--- web/modules/custom/dept_migrate/src/EventSubscriber/PreDomainAccessCleanUp.php
+++ web/modules/custom/dept_migrate/src/EventSubscriber/PreDomainAccessCleanUp.php
@@ -57,19 +57,21 @@ class PreDomainAccessCleanUp implements EventSubscriberInterface {
    *   The import event object.
    */
   public function onMigratePreImport(MigrateImportEvent $event) {
+    $event_id = $event->getMigration()->getBaseId();
 
-    // Delete nigov da rows for news items of type 'press release'.
-    $this->db7conn->query("DELETE domain_access FROM domain_access INNER JOIN field_data_field_news_type ON domain_access.nid = field_data_field_news_type.entity_id WHERE field_data_field_news_type.field_news_type_value = 'pressrelease' AND domain_access.gid = 1");
+    if (strpos($event_id, 'node_') === 0) {
+      // Delete nigov da rows for news items of type 'press release'.
+      $this->db7conn->query("DELETE domain_access FROM domain_access INNER JOIN field_data_field_news_type ON domain_access.nid = field_data_field_news_type.entity_id WHERE field_data_field_news_type.field_news_type_value = 'pressrelease' AND domain_access.gid = 1");
 
-    // Delete nigov da rows for publication and secure publications.
-    $this->db7conn->query("DELETE domain_access FROM domain_access INNER JOIN node ON domain_access.nid = node.nid WHERE domain_access.gid = 1 AND node.type = 'publication' OR node.type = 'secure_publication' ");
+      // Delete nigov da rows for publication and secure publications.
+      $this->db7conn->query("DELETE domain_access FROM domain_access INNER JOIN node ON domain_access.nid = node.nid WHERE domain_access.gid = 1 AND node.type = 'publication' OR node.type = 'secure_publication' ");
 
-    // Delete nigov da rows for consultations.
-    $this->db7conn->query("DELETE domain_access FROM domain_access INNER JOIN node ON domain_access.nid = node.nid WHERE domain_access.gid = 1 AND node.type = 'consultation' ");
+      // Delete nigov da rows for consultations.
+      $this->db7conn->query("DELETE domain_access FROM domain_access INNER JOIN node ON domain_access.nid = node.nid WHERE domain_access.gid = 1 AND node.type = 'consultation' ");
 
-    // Delete rows for old domains.
-    $this->db7conn->query("DELETE domain_access FROM domain_access WHERE domain_access.gid IN (3,10,11)");
-
+      // Delete rows for old domains.
+      $this->db7conn->query("DELETE domain_access FROM domain_access WHERE domain_access.gid IN (3,10,11)");
+    }
   }
 
 }
