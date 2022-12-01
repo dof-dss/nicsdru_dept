57a79f81b18d328210dd3d4f133fc963b796699b
Johan Gant <johan@jnca-consulting.com>
2022-12-01 15:01:22 +0000

    Add branch check in the script so we don't execute once per active environment

--- reindex-edge-solr.sh
+++ reindex-edge-solr.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 PLATFORM_SOLR_HOST=solr.internal:8080
 
+if [ ${PLATFORM_BRANCH} != "DEPT-edge" ];
+then
+  # Not running on the edge environment, exit.
+  exit 0
+fi
+
 echo "Force purge of Solr service contents..."
 curl http://${PLATFORM_SOLR_HOST}/solr/default/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
 curl http://${PLATFORM_SOLR_HOST}/solr/default/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
