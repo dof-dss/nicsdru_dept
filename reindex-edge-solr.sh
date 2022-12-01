02eaf0547adbe8f9e251a45b9bb7642565465340
Johan Gant <johan@jnca-consulting.com>
2022-12-01 14:55:50 +0000

    Add script file

---
+++ reindex-edge-solr.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+PLATFORM_SOLR_HOST=solr.internal:8080
+
+echo "Force purge of Solr service contents..."
+curl http://${PLATFORM_SOLR_HOST}/solr/default/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
+curl http://${PLATFORM_SOLR_HOST}/solr/default/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+
+echo "Reset Solr tracker index"
+drush sapi-rt
+echo "Clear Solr indexes"
+drush sapi-c
+echo "Mark all content for re-indexing"
+drush sapi-r
+echo "Reindex all active indices"
+drush sapi-i --batch-size=250
+
+echo "DONE"
