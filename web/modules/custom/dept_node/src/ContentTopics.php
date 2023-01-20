9083f37083a7e629b639cc250eab6cba4795adf3
Johan Gant <johan@jnca-consulting.com>
2023-01-20 15:52:28 +0000

    Add service function to generate subtopic content

--- web/modules/custom/dept_node/src/ContentTopics.php
+++ web/modules/custom/dept_node/src/ContentTopics.php
@@ -166,4 +166,53 @@ class ContentTopics {
     return $render;
   }
 
+  /**
+   * @param int|\Drupal\node\NodeInterface $node
+   *   The subtopic node (nid or full object)
+   *
+   * @return array
+   *   A render array of links elements.
+   */
+  public function getSubtopicContent(int|NodeInterface $node): array {
+    $content = [];
+
+    if (empty($node)) {
+      return $content;
+    }
+
+    if (is_int($node)) {
+      $node = $this->entityTypeManager->getStorage('node')->load($node);
+    }
+
+    if ($node instanceof NodeInterface && $node->bundle() === 'subtopic') {
+      // Fetch articles tagged with this subtopic, as well as subtopics
+      // referencing it from the parent subtopic field.
+      $subtopic_content_sql = "SELECT
+        st_nfd.nid,
+        st_nfd.type,
+        st_nfd.title
+        FROM {node_field_data} st_nfd
+        JOIN {node__field_parent_subtopic} nfps ON st_nfd.nid = nfps.entity_id
+        WHERE st_nfd.type = 'subtopic' AND nfps.field_parent_subtopic_target_id = :subtopic_id
+      UNION
+        SELECT
+        ar_nfd.nid,
+        ar_nfd.type,
+        ar_nfd.title
+        FROM {node_field_data} ar_nfd
+        JOIN {node__field_site_subtopics} nfss ON ar_nfd.nid = nfss.entity_id
+        WHERE ar_nfd.type = 'article' AND nfss.field_site_subtopics_target_id = :subtopic_id";
+
+      $subtopic_content = \Drupal::database()
+        ->query($subtopic_content_sql, [':subtopic_id' => $node->id()])
+        ->fetchAll();
+
+      foreach ($subtopic_content as $row) {
+        $content[] = Link::createFromRoute($row->title, 'entity.node.canonical', ['node' => $row->nid])->toString();
+      }
+    }
+
+    return $content;
+  }
+
 }
