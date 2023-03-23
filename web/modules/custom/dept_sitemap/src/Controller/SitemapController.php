5e6df5066672a353dc2534575c643b9ba46488ac
OMAHM <hello@onemanandhismouse.com>
2023-03-23 09:26:32 +0000

    DEPT-31 ~ Redirect sitemap requests to the current domain variant

---
+++ web/modules/custom/dept_sitemap/src/Controller/SitemapController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\dept_sitemap\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\domain\DomainNegotiator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Returns responses for Departmental sites: Sitemaps routes.
+ */
+class SitemapController extends ControllerBase {
+
+  /**
+   * @var \Drupal\domain\DomainNegotiator
+   */
+  protected $domainNegotiator;
+
+  public function __construct(DomainNegotiator $domain_negotiator) {
+    $this->domainNegotiator = $domain_negotiator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('domain.negotiator')
+    );
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function build() {
+    $domain_id = $this->domainNegotiator->getActiveDomain()->id();
+    return $this->redirect('simple_sitemap.sitemap_variant', ['variant' => $domain_id]);
+  }
+
+}
