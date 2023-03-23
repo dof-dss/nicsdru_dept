1601a1c8584f93e3b74e18ba1ddce6a3fdd9f822
OMAHM <hello@onemanandhismouse.com>
2023-03-23 13:31:54 +0000

    DEPT-31 ~ Additional sitemap detail columns

--- web/modules/custom/dept_sitemap/src/Controller/SitemapAdminController.php
+++ web/modules/custom/dept_sitemap/src/Controller/SitemapAdminController.php
@@ -3,6 +3,7 @@
 namespace Drupal\dept_sitemap\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\simple_sitemap\Entity\SimpleSitemap;
 
 /**
  * Returns responses for Departmental sites: Sitemaps routes.
@@ -23,17 +24,38 @@ class SitemapAdminController extends ControllerBase {
 
     $active_sitemaps = $this->entityTypeManager()->getStorage('simple_sitemap')->loadByProperties(['status' => 'true']);
 
-    foreach ($dept_domains as $domain) {
+    foreach ($dept_domains as $id => $domain) {
       if (array_key_exists($domain->id(), $active_sitemaps)) {
-        $rows[] = [$domain->label(), 'true'];
+        $rows[$id] = ['title' => $domain->label(), 'sitemap' => $this->t('Yes'), 'links' => $active_sitemaps[$id]->getLinkCount(), 'status' => $this->t('Pending')];
+
+        /** @var \Drupal\simple_sitemap\Entity\SimpleSitemapInterface $entity */
+        if ($active_sitemaps[$id]->fromPublishedAndUnpublished()->getChunkCount()) {
+          switch ($active_sitemaps[$id]->contentStatus()) {
+
+            case SimpleSitemap::SITEMAP_UNPUBLISHED:
+              $rows[$id]['status'] = $this->t('Generating');
+              break;
+
+            case SimpleSitemap::SITEMAP_PUBLISHED:
+            case SimpleSitemap::SITEMAP_PUBLISHED_GENERATING:
+              $created = \Drupal::service('date.formatter')->format($active_sitemaps[$id]->fromPublished()->getCreated());
+              $rows[$id]['status'] = $active_sitemaps[$id]->contentStatus() === SimpleSitemap::SITEMAP_PUBLISHED
+                ? $this->t('Published on @time', ['@time' => $created])
+                : $this->t('Published on @time, regenerating', ['@time' => $created]);
+              break;
+          }
+        }
+
       } else {
-        $rows[] = [$domain->label(), 'false'];
+        $rows[$id] = ['department' => $domain->label(), 'sitemap' => $this->t('No'), 'links' => 0, 'status' => ''];
       }
     }
 
     $header = [
       $this->t('Department'),
       $this->t('Sitemap'),
+      $this->t('Link count'),
+      $this->t('Status'),
     ];
 
     $build['content'] = [
