0fcb0b9fbf3c892d8a7e08df0de736e82201006f
OMAHM <hello@onemanandhismouse.com>
2023-03-23 13:58:50 +0000

    DEPT-31 ~ Add operations, refactor row creation

--- web/modules/custom/dept_sitemap/src/Controller/SitemapAdminController.php
+++ web/modules/custom/dept_sitemap/src/Controller/SitemapAdminController.php
@@ -3,6 +3,7 @@
 namespace Drupal\dept_sitemap\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 use Drupal\simple_sitemap\Entity\SimpleSitemap;
 
 /**
@@ -14,9 +15,8 @@ class SitemapAdminController extends ControllerBase {
    * Builds the response.
    */
   public function build() {
-
-
     $dept_domains = $this->entityTypeManager()->getStorage('domain')->loadByProperties(['status' => 'true']);
+    $rows = [];
 
     if (array_key_exists('dept_admin', $dept_domains)) {
       unset($dept_domains['dept_admin']);
@@ -25,30 +25,69 @@ class SitemapAdminController extends ControllerBase {
     $active_sitemaps = $this->entityTypeManager()->getStorage('simple_sitemap')->loadByProperties(['status' => 'true']);
 
     foreach ($dept_domains as $id => $domain) {
+      $row = [
+        'department' => $domain->label(),
+        'sitemap' => $this->t('No'),
+        'link_total' => '',
+        'status' => '',
+      ];
+
+      $links = [];
+
       if (array_key_exists($domain->id(), $active_sitemaps)) {
-        $rows[$id] = ['title' => $domain->label(), 'sitemap' => $this->t('Yes'), 'links' => $active_sitemaps[$id]->getLinkCount(), 'status' => $this->t('Pending')];
+        $row['sitemap'] = $this->t('Yes');
+        $row['link_total'] = $active_sitemaps[$id]->getLinkCount();
+        $row['status'] = $this->t('Pending');
+
 
         /** @var \Drupal\simple_sitemap\Entity\SimpleSitemapInterface $entity */
         if ($active_sitemaps[$id]->fromPublishedAndUnpublished()->getChunkCount()) {
           switch ($active_sitemaps[$id]->contentStatus()) {
 
             case SimpleSitemap::SITEMAP_UNPUBLISHED:
-              $rows[$id]['status'] = $this->t('Generating');
+              $row['status'] = $this->t('Generating');
               break;
 
             case SimpleSitemap::SITEMAP_PUBLISHED:
             case SimpleSitemap::SITEMAP_PUBLISHED_GENERATING:
               $created = \Drupal::service('date.formatter')->format($active_sitemaps[$id]->fromPublished()->getCreated());
-              $rows[$id]['status'] = $active_sitemaps[$id]->contentStatus() === SimpleSitemap::SITEMAP_PUBLISHED
+            $row['status'] = $active_sitemaps[$id]->contentStatus() === SimpleSitemap::SITEMAP_PUBLISHED
                 ? $this->t('Published on @time', ['@time' => $created])
                 : $this->t('Published on @time, regenerating', ['@time' => $created]);
               break;
           }
         }
 
-      } else {
-        $rows[$id] = ['department' => $domain->label(), 'sitemap' => $this->t('No'), 'links' => 0, 'status' => ''];
+        $links['view'] = [
+          'title' => t('View'),
+          'url' => Url::fromRoute('simple_sitemap.sitemap_variant', [
+            'variant' => $id,
+          ]),
+        ];
+        $row['operations'] = [
+          'data' => [
+            '#type' => 'operations',
+            '#links' => $links,
+          ],
+        ];
+
       }
+      else {
+        $links['create'] = [
+          'title' => t('Create'),
+          'url' => Url::fromRoute('simple_sitemap.sitemap_variant', [
+            'variant' => $id,
+          ]),
+        ];
+        $row['operations'] = [
+          'data' => [
+            '#type' => 'operations',
+            '#links' => $links,
+          ],
+        ];
+      }
+
+      $rows[] = $row;
     }
 
     $header = [
@@ -56,6 +95,7 @@ class SitemapAdminController extends ControllerBase {
       $this->t('Sitemap'),
       $this->t('Link count'),
       $this->t('Status'),
+      $this->t('Operations'),
     ];
 
     $build['content'] = [
