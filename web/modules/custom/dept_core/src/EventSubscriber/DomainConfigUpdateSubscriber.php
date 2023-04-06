34a2cf60aeea335e07c528be8281951c05209e40
OMAHM <hello@onemanandhismouse.com>
2023-04-06 14:04:58 +0100

    DEPT-423 ~ Update Department label when Domain config is updated

---
+++ web/modules/custom/dept_core/src/EventSubscriber/DomainConfigUpdateSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\dept_core\EventSubscriber;
+
+use Drupal\Core\Config\ConfigEvents;
+use Drupal\Core\Config\ConfigImporterEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+
+/**
+ * Departmental sites: core event subscriber.
+ */
+class DomainConfigUpdateSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Config import event handler.
+   *
+   * @param \Drupal\Core\Config\ConfigImporterEvent $event
+   *   Config import event.
+   */
+  public function onConfigImport(ConfigImporterEvent $event) {
+    $change_list = $event->getChangelist('update');
+
+    $changes = $event->getConfigImporter()->getProcessedConfiguration();
+
+    if (!empty($changes['update'])) {
+      $domain_updates = array_filter($changes['update'], function ($value, $key) {
+        return str_starts_with($value, 'domain.record.');
+      }, ARRAY_FILTER_USE_BOTH);
+
+    }
+
+    foreach ($domain_updates as $update) {
+      $id = \Drupal::config($update)->get('id');
+      $name = \Drupal::config($update)->get('name');
+
+      $dept = \Drupal::entityTypeManager()->getStorage('department')->load($id);
+      $dept->set('label', $name);
+      $dept->save();
+    }
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ConfigEvents::IMPORT => ['onConfigImport'],
+    ];
+  }
+
+}
