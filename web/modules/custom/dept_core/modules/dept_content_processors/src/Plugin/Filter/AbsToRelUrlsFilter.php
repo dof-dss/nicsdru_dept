df83ad044e9d4fc5b5d849e2ba99357b5a5bf3e1
Johan Gant <johan@jnca-consulting.com>
2023-04-21 15:21:01 +0100

    Replace constructor injection of dept manager service with instance approach

--- web/modules/custom/dept_core/modules/dept_content_processors/src/Plugin/Filter/AbsToRelUrlsFilter.php
+++ web/modules/custom/dept_core/modules/dept_content_processors/src/Plugin/Filter/AbsToRelUrlsFilter.php
@@ -31,33 +31,17 @@ class AbsToRelUrlsFilter extends FilterBase implements ContainerFactoryPluginInt
   protected $departmentManager;
 
   /**
-   * Constructs a MediaEmbed object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin ID for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\dept_core\DepartmentManager $department_manager
-   *   The department manager object.
-   *
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, DepartmentManager $department_manager) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->departmentManager = $department_manager;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
+    $instance = new static(
       $configuration,
       $plugin_id,
-      $plugin_definition,
-      $container->get('department.manager'),
+      $plugin_definition
     );
+    $instance->departmentManager = $container->get('department.manager');
+
+    return $instance;
   }
 
   /**
