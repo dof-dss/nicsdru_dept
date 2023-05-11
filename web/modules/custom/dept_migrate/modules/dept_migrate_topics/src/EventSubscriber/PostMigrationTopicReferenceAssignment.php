835b27304d135505cbf9b020bb1e9b09de41b7f2
OMAHM <hello@onemanandhismouse.com>
2023-05-11 16:30:24 +0100

    DEPT-446 ~ Import perf by building reference delta array for topic nodes

--- web/modules/custom/dept_migrate/modules/dept_migrate_topics/src/EventSubscriber/PostMigrationTopicReferenceAssignment.php
+++ web/modules/custom/dept_migrate/modules/dept_migrate_topics/src/EventSubscriber/PostMigrationTopicReferenceAssignment.php
@@ -74,36 +74,46 @@ class PostMigrationTopicReferenceAssignment implements EventSubscriberInterface 
 
     if ($event_id === 'node_subtopic') {
 
-      $subtopics =  $this->dbconn->query("
-        SELECT pt.entity_id AS subtopic, pt.field_parent_topic_target_id AS topic, n.vid AS revision FROM node__field_parent_topic pt
+      $subtopics = $this->dbconn->query("
+        SELECT pt.entity_id AS subtopic,
+               pt.field_parent_topic_target_id AS topic,
+               n.vid AS revision, nfd.status
+        FROM node__field_parent_topic pt
         LEFT JOIN node n
-        ON pt.entity_id = n.nid
+        ON pt.field_parent_topic_target_id = n.nid
+        LEFT JOIN node_field_data nfd
+        ON pt.entity_id = nfd.nid
         LEFT JOIN node__field_topic_content tc
-        ON pt.field_parent_topic_target_id = tc.entity_id AND pt.entity_id = tc.field_topic_content_target_id
-        WHERE tc.entity_id IS NULL"
-      )->fetchAll();
+        ON pt.field_parent_topic_target_id = tc.entity_id
+        AND pt.entity_id = tc.field_topic_content_target_id
+        WHERE nfd.status = 1 AND tc.entity_id IS NULL
+      ")->fetchAll();
+
+      $deltas = $this->dbconn->query("
+        SELECT entity_id, MAX(delta)
+        FROM node__field_topic_content
+        GROUP BY entity_id")->fetchAllKeyed();
 
       foreach ($subtopics as $subtopic) {
-        $delta = $this->dbconn->query("
-          SELECT MAX(delta) + 1
-          FROM node__field_topic_content
-          WHERE entity_id = " . $subtopic->topic
-        )->fetchField();
 
-        $delta = $delta ?? 0;
+        if (array_key_exists($subtopic->topic, $deltas)) {
+          $deltas[$subtopic->topic]++;
+        } else {
+          $deltas[$subtopic->topic] = 0;
+        }
 
-        $this->dbconn->insert('node__field_topic_content')->fields([
-          'bundle' => 'Topic',
-          'deleted' => 0,
-          'entity_id' => $subtopic->topic,
-          'revision_id' => $subtopic->revision,
-          'langcode' => 'en',
-          'delta' => $delta,
-          'field_topic_content_target_id' => $subtopic->subtopic,
-        ])
-        ->execute();
+        $this->dbconn->insert('node__field_topic_content')
+          ->fields([
+            'bundle' => 'Topic',
+            'deleted' => 0,
+            'entity_id' => $subtopic->topic,
+            'revision_id' => $subtopic->revision,
+            'langcode' => 'en',
+            'delta' => $deltas[$subtopic->topic],
+            'field_topic_content_target_id' => $subtopic->subtopic,
+          ])
+          ->execute();
       }
     }
   }
-
 }
