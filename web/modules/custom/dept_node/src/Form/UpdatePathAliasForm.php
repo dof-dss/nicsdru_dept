7d2a7d0ad72c1195109359b25f4284900d2fae71
OMAHM <hello@onemanandhismouse.com>
2023-06-08 15:52:22 +0100

    DEPT-463 ~ Replace submit handler with callback to ensure ajax response from validation

--- web/modules/custom/dept_node/src/Form/UpdatePathAliasForm.php
+++ web/modules/custom/dept_node/src/Form/UpdatePathAliasForm.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\dept_node\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\RedirectCommand;
+use Drupal\Core\Ajax\ReplaceCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides a Departmental sites: node form.
@@ -34,6 +38,9 @@ class UpdatePathAliasForm extends FormBase {
 
     $results = $dbConn->query("SELECT pa.path, pa.alias FROM path_alias pa WHERE pa.path = :source", [':source' => $source])->fetchCol(1);
 
+    $form['#prefix'] = '<div id="update-path-alias-form">';
+    $form['#suffix'] = '</div>';
+
     $form['nid'] = [
       '#type' => 'hidden',
       '#value' => $nid,
@@ -46,32 +53,72 @@ class UpdatePathAliasForm extends FormBase {
       '#required' => TRUE,
     ];
 
+    $form['status_messages'] = [
+      '#type' => 'status_messages',
+      '#weight' => -10,
+    ];
+
     $form['actions'] = [
       '#type' => 'actions',
     ];
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Update'),
+      '#attributes' => [
+        'class' => [
+          'use-ajax',
+        ],
+      ],
+      '#ajax' => [
+        'callback' => [$this, 'submitFormAjax'],
+        'event' => 'click',
+      ],
     ];
 
+    $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
+
     return $form;
+  }
+
+
+  public function submitFormAjax(array $form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+
+    // If there are any form errors, AJAX replace the form.
+    if ($form_state->hasAnyErrors()) {
+      $response->addCommand(new ReplaceCommand('#update-path-alias-form', $form));
+    }
+    else {
+      $dbConn = \Drupal::database();
+      $node_path = '/node/' . $form_state->getValue('nid');
+
+      $dbConn->update('path_alias')
+        ->fields(['alias' => $form_state->getValue('new_alias')])
+        ->condition('path', $node_path , '=')
+        ->execute();
+
+      $response->addCommand(new RedirectCommand(Url::fromRoute('entity.node.canonical', ['node' => $form_state->getValue('nid')])->toString()));
+    }
+
+    return $response;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $dbConn = \Drupal::database();
+    $results = $dbConn->query("SELECT pa.path, pa.alias FROM path_alias pa WHERE pa.alias = :alias", [':alias' => $form_state->getValue('new_alias')])->fetchCol(1);
+
+    if ($results) {
+      $form_state->setErrorByName('new_alias', 'Alias already in use, please try another');
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $dbConn = \Drupal::database();
-    $node_path = '/node/' . $form_state->getValue('nid');
-
-    $results = $dbConn->query("SELECT pa.path, pa.alias FROM path_alias pa WHERE pa.path = :source", [':source' => $source])->fetchCol(1);
-
-    $result = $dbConn->update('path_alias')
-           ->fields(['alias' => $form_state->getValue('new_alias')])
-           ->condition('path', $node_path , '=')
-           ->execute();
-
-    $form_state->setRedirect('entity.node.canonical', ['node' => $form_state->getValue('nid')]);
   }
 
 }
