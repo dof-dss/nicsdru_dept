b3fea305d5bb02f83372e8486f7552d94a69f60e
OMAHM <hello@onemanandhismouse.com>
2023-06-13 21:36:40 +0100

    DEPT-442 ~ Create Redirects d7 page urls

--- web/modules/custom/dept_migrate/src/EventSubscriber/RedirectMigrateSubscriber.php
+++ web/modules/custom/dept_migrate/src/EventSubscriber/RedirectMigrateSubscriber.php
@@ -2,12 +2,15 @@
 
 namespace Drupal\dept_migrate\EventSubscriber;
 
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\dept_migrate\MigrateUuidLookupManager;
-use Drupal\migrate_plus\Event\MigrateEvents;
-use Drupal\migrate\Event\MigratePreRowSaveEvent;
+use Drupal\migrate\Event\MigrateImportEvent;
+use Drupal\migrate_plus\Event\MigrateEvents as MigratePlusEvents;
+use Drupal\migrate\Event\MigrateEvents;
 use Drupal\migrate_plus\Event\MigratePrepareRowEvent;
+use Drupal\redirect\Entity\Redirect;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -33,20 +36,42 @@ class RedirectMigrateSubscriber implements EventSubscriberInterface {
   protected $logger;
 
   /**
+   * Database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $dbconn;
+
+
+  /**
+   * Database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $d7dbconn;
+
+  /**
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   Entity type manager service.
    * @param \Drupal\dept_migrate\MigrateUuidLookupManager $lookup_manager
    *   Lookup Manager.
    * @param \Drupal\Core\Logger\LoggerChannelFactory $logger
    *   Drupal logger.
+   * @param \Drupal\Core\Database\Connection $connection
+   *   Database connection.
+   * @param \Drupal\Core\Database\Connection $d7_connection
+   *   Database connection.
    */
   public function __construct(EntityTypeManagerInterface $entity_type_manager,
                               MigrateUuidLookupManager $lookup_manager,
-                              LoggerChannelFactory $logger) {
-
+                              LoggerChannelFactory $logger,
+                              Connection $connection,
+                              Connection $d7_connection) {
     $this->entityTypeManager = $entity_type_manager;
     $this->lookupManager = $lookup_manager;
     $this->logger = $logger->get('dept_migrate');
+    $this->dbconn = $connection;
+    $this->d7dbconn = $d7_connection;
   }
 
   /**
@@ -91,12 +116,46 @@ class RedirectMigrateSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * Handle post import migration event.
+   *
+   * @param \Drupal\migrate\Event\MigrateImportEvent $event
+   *   The import event object.
+   */
+  public function onMigratePostImport(MigrateImportEvent $event) {
+    $event_id = $event->getMigration()->getBaseId();
+
+    if ($event_id === 'node_page') {
+      $nids = $this->d7dbconn->query("SELECT n.nid, a.alias FROM {node} n LEFT JOIN {url_alias} a ON CONCAT('node/', n.nid) = a.source WHERE n.type = 'page'")
+        ->fetchAllKeyed();
+
+      foreach ($nids as $nid => $alias) {
+        $d9_info = $this->lookupManager->lookupBySourceNodeId([$nid]);
+
+        if (is_array($d9_info)) {
+          $d9_info = current($d9_info);
+
+          if (is_array($d9_info) && array_key_exists('nid', $d9_info)) {
+            $redirect = Redirect::create([
+              'redirect_source' => $alias,
+              'redirect_redirect' => 'internal:/node/' . $d9_info['nid'],
+              'language' => 'und',
+              'status_code' => '301',
+            ]);
+            $redirect->save();
+          }
+        }
+      }
+    }
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() : array {
     $events = [];
     // NB: MigrateEvents is a migrate_plus namespace.
-    $events[MigrateEvents::PREPARE_ROW] = ['onPrepareRow'];
+    $events[MigratePlusEvents::PREPARE_ROW] = ['onPrepareRow'];
+    $events[MigrateEvents::POST_IMPORT] = ['onMigratePostImport'];
     return $events;
   }
 
