c673124875492aaaf012b357ddb9cce927aa7ef1
OMAHM <hello@onemanandhismouse.com>
2023-06-27 11:40:54 +0100

    DEPT-457 ~ Annotation for Dept entity field helper methods

---
+++ web/modules/custom/dept_core/src/Annotations/DepartmentDetails.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\dept_core\Annotations;
+
+/**
+ * Provides an annotation to Department entity field helper methods.
+ *
+ * @Annotation
+ * @Target({"METHOD"})
+ */
+class DepartmentDetails {
+
+  /**
+   * Field label.
+   *
+   * @var string|mixed
+   */
+  private string $label;
+
+  /**
+   * Annotation contructor.
+   *
+   * @param array $values
+   *   Annotation values.
+   */
+  public function __construct(array $values) {
+    $this->label = $values['label'];
+  }
+
+  /**
+   * Field label.
+   *
+   * @return string
+   *   Returns a formatted field label.
+   */
+  public function label() {
+    return ucfirst(trim($this->label));
+  }
+
+}
