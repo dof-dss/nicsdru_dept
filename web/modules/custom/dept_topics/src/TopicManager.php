09499bd2c9e96eaa1a8c10ad37a184b883be4e39
OMAHM <hello@onemanandhismouse.com>
2023-08-29 12:15:22 +0100

    DEPT-527 ~ Refactoring and topictags method for topic manager

--- web/modules/custom/dept_topics/src/TopicManager.php
+++ web/modules/custom/dept_topics/src/TopicManager.php
@@ -8,19 +8,11 @@ use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\node\NodeInterface;
-use Drupal\node\NodeStorage;
 
 /**
  * Provides methods for managing Sub/Topic referenced (child) content.
  */
 class TopicManager {
-
-  /**
-   * Array of parent nodes.
-   *
-   * @var array
-   */
-  protected $parents = [];
 
   /**
    * Array of department topics.
@@ -98,7 +90,7 @@ class TopicManager {
   }
 
   /**
-   * Returns parent nodes for the given node.
+   * Returns parent topics for the given node.
    *
    * @param NodeInterface|int $node
    *   Node or node ID to return the parent for.
@@ -106,27 +98,67 @@ class TopicManager {
    * @return array|mixed
    *   Node ID indexed array comprising id, title and type.
    */
-  public function getParentNodes($node) {
+  public function getTopicTags($node) {
+    $topic_tag_nodes = [];
 
-    $nid = ($node instanceof NodeInterface) ? $node->id() : $node;
+    if ($node->hasField('field_site_topics')) {
+      $topics = $node->get('field_site_topics')->getValue();
+    }
 
-    $parents = $this->dbConn->query("SELECT n.nid, nfd.title, nfd.type FROM node n
+    foreach ($topics as $topic) {
+      $topic = $this->entityTypeManager->getStorage('node')->load($topic['target_id']);
+      $topic_tag_nodes[$topic->id()] = $topic;
+
+      $parents = $this->getParentNodes($topic);
+
+      foreach ($parents as $parent) {
+        if ($parent->type === 'topic') {
+          $parent = $this->entityTypeManager->getStorage('node')->load($parent->nid);
+          $topic_tag_nodes[$parent->id()] = $parent;
+        }
+      }
+    }
+
+    return $topic_tag_nodes;
+  }
+
+  /**
+   * Returns parent nodes for the given node ID.
+   *
+   * @param NodeInterface|int $node
+   *   Node or Node ID to return the parents for.
+   *
+   * @param array $parents
+   *   Array of existing parent nodes.
+   *
+   * @return array|mixed
+   *   Node ID indexed array comprising id, title and type.
+   */
+  public function getParentNodes($node, &$parents = []) {
+
+    if ($node instanceof NodeInterface) {
+      $nid = $node->id();
+    } else {
+      $nid = $node;
+    }
+
+    $nodes = $this->dbConn->query("SELECT n.nid, nfd.title, nfd.type FROM node n
         LEFT JOIN node_field_data nfd
         ON nfd.nid = n.nid
         LEFT JOIN node__field_topic_content ftc
         ON ftc.entity_id = n.nid
         WHERE ftc.field_topic_content_target_id = :nid", [':nid' => $nid])->fetchAllAssoc('nid');
 
-    if ($parents === NULL) {
-      return $this->parents;
+    if ($nodes === NULL) {
+      return $parents;
     }
 
-    foreach ($parents as $parent) {
-      $this->parents[$parent->nid] = $parent;
-      $this->getParentNodes($parent->nid);
+    foreach ($nodes as $node) {
+      $parents[$node->nid] = $node;
+      $this->getParentNodes($node->nid, $parents);
     }
 
-    return $this->parents;
+    return $parents;
   }
 
   /**
