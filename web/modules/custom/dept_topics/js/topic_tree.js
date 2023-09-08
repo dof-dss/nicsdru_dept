f8800a3324596c581908d41a866ec634d07ed78c
OMAHM <hello@onemanandhismouse.com>
2023-09-08 11:18:21 +0100

    DEPT-542 ~ Wrap topic tree js in once call

--- web/modules/custom/dept_topics/js/topic_tree.js
+++ web/modules/custom/dept_topics/js/topic_tree.js
@@ -6,69 +6,74 @@
 (function($, Drupal, drupalSettings) {
   Drupal.behaviors.topicTree = {
     attach: function(context, settings) {
-      let select_field = "#" + drupalSettings["topic_tree.field"];
 
-      $('#topic-tree-wrapper')
-        .on('changed.jstree', function (e, data) {
-          // Update the hidden form field values when tree changes.
-          $('input[name="selected_topics"]').val(data.instance.get_selected());
-        })
-        .on("ready.jstree", function(e, data) {
-          // Check all tree elements matching the selected options.
-          $(select_field + " input:checked").each(function () {
-            data.instance.select_node($(this).val());
-          });
-        })
-        .on("select_node.jstree", function(e, data) {
-          // Deselect all parents.
-          for (const [key, value] of Object.entries(data.node.parents)) {
-            data.instance.deselect_node(value);
-          }
+      $(once('topic-tree', '#topic-tree-wrapper', context)).each(function () {
 
-          // Deselect all children.
-          for (const [key, value] of Object.entries(data.node.children_d)) {
-            data.instance.deselect_node(value);
-          }
+        let select_field = "#" + drupalSettings["topic_tree.field"];
 
-          // Warn when hitting the topic selection limit.
-          if (data.instance.get_selected().length > drupalSettings["topic_tree.limit"]) {
-            data.instance.deselect_node(data.node);
-            alert('Topic selection limit reached.')
-          }
+        $(this)
+          .on('changed.jstree', function (e, data) {
+            // Update the hidden form field values when tree changes.
+            $('input[name="selected_topics"]').val(data.instance.get_selected());
+          })
+          .on("ready.jstree", function(e, data) {
+            // Check all tree elements matching the selected options.
+            $(select_field + " input:checked").each(function () {
+              data.instance.select_node($(this).val());
+            });
+          })
+          .on("select_node.jstree", function(e, data) {
+            // Deselect all parents.
+            for (const [key, value] of Object.entries(data.node.parents)) {
+              data.instance.deselect_node(value);
+            }
 
-          $('#topic-tree-count span').text(data.instance.get_selected().length);
-        })
-        .jstree({
-          core: {
-            data: {
-              url: function(node) {
-                return Drupal.url(
-                  "admin/topics/topic_tree/json/" + drupalSettings["topic_tree.department"]
-                );
+            // Deselect all children.
+            for (const [key, value] of Object.entries(data.node.children_d)) {
+              data.instance.deselect_node(value);
+            }
+
+            // Warn when hitting the topic selection limit.
+            if (data.instance.get_selected().length > drupalSettings["topic_tree.limit"]) {
+              data.instance.deselect_node(data.node);
+              alert('Topic selection limit reached.')
+            }
+
+            $('#topic-tree-count span').text(data.instance.get_selected().length);
+          })
+          .jstree({
+            core: {
+              data: {
+                url: function(node) {
+                  return Drupal.url(
+                    "admin/topics/topic_tree/json/" + drupalSettings["topic_tree.department"]
+                  );
+                },
+                data: function(node) {
+                  return {
+                    id: node.id,
+                    text: node.text,
+                    parent: node.parent
+                  };
+                }
               },
-              data: function(node) {
-                return {
-                  id: node.id,
-                  text: node.text,
-                  parent: node.parent
-                };
-              }
             },
-          },
-          checkbox: {
-            three_state: false
-          },
-          plugins: ["changed", "checkbox", "search"],
-          "search": {
-            "case_sensitive": false,
-            "show_only_matches": true,
-          }
-        });
+            checkbox: {
+              three_state: false
+            },
+            plugins: ["changed", "checkbox", "search"],
+            "search": {
+              "case_sensitive": false,
+              "show_only_matches": true,
+            }
+          });
 
-      $('#topic-tree-search').keyup(function() {
-        let search_text = $(this).val();
-        $('#topic-tree-wrapper').jstree('search', search_text);
+        $('#topic-tree-search').keyup(function() {
+          let search_text = $(this).val();
+          $('#topic-tree-wrapper').jstree('search', search_text);
+        });
       });
+
     }
   }
 })(jQuery, Drupal, drupalSettings);
