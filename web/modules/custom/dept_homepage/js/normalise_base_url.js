6226ff6122ba718739ef3008688f10843b294eea
Johan Gant <johan.gant@gmail.com>
2024-07-19 11:36:35 +0100

    Fix hostname replace to include protocol as well

--- web/modules/custom/dept_homepage/js/normalise_base_url.js
+++ web/modules/custom/dept_homepage/js/normalise_base_url.js
@@ -11,12 +11,13 @@
     attach: function (context, settings) {
       $('a[href^="http"]:not(#toolbar-item-sites-tray a)').each(function (index, linkElement) {
         let href = $(linkElement).attr('href');
+        const currentDeptUrl = $(location).attr('origin');
         const currentDeptHostname = $(location).attr('host');
 
         // Absolute link, check if it matches our dept hostname.
         if (href.indexOf(currentDeptHostname) < 0) {
           // Not found/different, so adjust the hostname to the current dept.
-          href = href.replace(href, currentDeptHostname);
+          href = href.replace(href, currentDeptUrl);
           $(linkElement).attr('href', href);
         }
       });
