3ffeee7cbcdb84d2cc4f7436c431b1d97d7a5c35
Johan Gant <johan.gant@gmail.com>
2024-07-19 15:38:38 +0100

    Attach custom JS to toolbar render element to fix hostname discrepancies (#849)
    * Attach custom JS to toolbar render element to fix hostname discrepancies
    
    * Fix hostname replace to include protocol as well

---
+++ web/modules/custom/dept_homepage/js/normalise_base_url.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Javascript behaviors for adjusting the toolbar's base url
+ * where domain route caching might have failed.
+ */
+(function ($) {
+
+  "use strict";
+
+  Drupal.behaviors.normaliseBaseUrl = {
+    attach: function (context, settings) {
+      $('a[href^="http"]:not(#toolbar-item-sites-tray a)').each(function (index, linkElement) {
+        let href = $(linkElement).attr('href');
+        const currentDeptUrl = $(location).attr('origin');
+        const currentDeptHostname = $(location).attr('host');
+
+        // Absolute link, check if it matches our dept hostname.
+        if (href.indexOf(currentDeptHostname) < 0) {
+          // Not found/different, so adjust the hostname to the current dept.
+          href = href.replace(href, currentDeptUrl);
+          $(linkElement).attr('href', href);
+        }
+      });
+    },
+  }
+
+}(jQuery, Drupal));
