447b9c44a1b642aa624dd41a4b6fe0d5ea92113c
OMAHM <hello@onemanandhismouse.com>
2024-09-18 20:47:55 +0100

    DEPT-890 ~ Migration command for topics. Command to populate top level topic content

---
+++ web/modules/custom/dept_migrate/src/Commands/DeptTopicMigrationCommands.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace Drupal\dept_migrate\Commands;
+
+use Consolidation\SiteAlias\SiteAliasManagerAwareTrait;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\dept_core\DepartmentManager;
+use Drupal\dept_migrate\LookupHelper;
+use Drush\Commands\DrushCommands;
+use Drush\SiteAlias\SiteAliasManagerAwareInterface;
+
+/**
+ * Drush commands processing Departmental migrations.
+ */
+class DeptTopicMigrationCommands extends DrushCommands implements SiteAliasManagerAwareInterface {
+
+  use StringTranslationTrait;
+  use SiteAliasManagerAwareTrait;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $dbConn;
+
+  /**
+   * Database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $d7conn;
+
+  /**
+   * Migration Lookup Manager.
+   *
+   * @var \Drupal\dept_migrate\LookupHelper
+   */
+  protected $lookupHelper;
+
+  /**
+   * EntityTypeManager service object.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Department manager service object.
+   *
+   * @var \Drupal\dept_core\DepartmentManager
+   */
+  protected $departmentManager;
+
+  /**
+   * Command constructor.
+   */
+  public function __construct(Connection $database, Connection $d7_database, LookupHelper $lookup_helper, EntityTypeManagerInterface $etm, DepartmentManager $dept_manager) {
+    parent::__construct();
+    $this->dbConn = $database;
+    $this->d7conn = $d7_database;
+    $this->lookupHelper = $lookup_helper;
+    $this->entityTypeManager = $etm;
+    $this->departmentManager = $dept_manager;
+  }
+
+  /**
+   * Create Topic sub contents.
+   *
+   * @command dept:topics-child-contents
+   * @aliases tcc
+   */
+  public function topicChildContents($domain_id) {
+
+    if (empty($domain_id)) {
+      return;
+    }
+
+    // Remove existing content reference links for the given department.
+    $this->dbConn->query("DELETE tc FROM node__field_topic_content tc
+      LEFT JOIN node__field_domain_source ds
+      ON tc.entity_id = ds.entity_id
+      WHERE ds.field_domain_source_target_id = '" . $domain_id . "'
+      AND tc.bundle = 'topic'"
+    )->execute();
+
+    $domain_topic_ids = $this->dbConn->query("
+        SELECT ds.entity_id AS nid FROM node__field_domain_source ds
+        INNER JOIN node_field_data nfd
+        ON ds.entity_id = nfd.nid
+        WHERE ds.bundle = 'topic'
+        AND ds.field_domain_source_target_id = '$domain_id'"
+    )->fetchCol(0);
+
+    $domain_topics = $this->entityTypeManager->getStorage('node')->loadMultiple(array_values($domain_topic_ids));
+
+    foreach ($domain_topics as $domain_topic) {
+      $child_contents = $this->subtopicsByTopic($domain_topic->id());
+      if ($child_contents) {
+        $delta = 0;
+
+        /* @var $child_content \Drupal\dept_migrate\LookupEntry */
+        foreach ($child_contents as $child_content) {
+
+          $child_content_exists = $this->dbConn->query("SELECT
+          * FROM {node__field_topic_content}
+          WHERE entity_id = :entity_id
+          AND revision_id = :rev_id
+          AND field_topic_content_target_id = :target_id", [
+            ':entity_id' => $domain_topic->id(),
+            ':rev_id' => $domain_topic->getRevisionId(),
+            ':target_id' => $child_content->id(),
+          ])->fetchAll();
+
+          if (empty($child_content_exists)) {
+            $this->dbConn->insert('node__field_topic_content')
+              ->fields([
+                'bundle' => 'topic',
+                'deleted' => 0,
+                'entity_id' => $domain_topic->id(),
+                'revision_id' => $domain_topic->getRevisionId(),
+                'langcode' => 'en',
+                'delta' => $delta++,
+                'field_topic_content_target_id' => $child_content->id(),
+              ])
+              ->execute();
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Preview content of a topic.
+   *
+   * @command dept:topics-child-contents
+   * @aliases ptcc
+   */
+  public function previewTopicChildContents($topic_id) {
+    print_r($this->subtopicsByTopic($topic_id));
+  }
+
+  /**
+   * Returns child content for top level topics.
+   *
+   * @param $topic_id
+   *
+   * @return array
+   */
+  private function subtopicsByTopic($topic_id) {
+    // Lookup the D7 nid for the given D10 topic id.
+    $topic_id = $this->lookupHelper->destination()->id($topic_id)->d7_id();
+
+    $sql = "SELECT node.nid AS nid, node.title AS node_title, node.created AS node_created, COALESCE(draggableviews_structure.weight, 2147483647) AS draggableviews_structure_weight_coalesce
+            FROM node
+            LEFT JOIN flagging flagging_node ON node.nid = flagging_node.entity_id AND (flagging_node.fid = '5')
+            LEFT JOIN domain_access domain_access ON node.nid = domain_access.nid AND (domain_access.realm = 'domain_id')
+            LEFT JOIN field_data_field_parent_subtopic field_data_field_parent_subtopic ON node.nid = field_data_field_parent_subtopic.entity_id AND (field_data_field_parent_subtopic.entity_type = 'node' AND field_data_field_parent_subtopic.deleted = '0')
+            LEFT JOIN field_data_field_parent_topic field_data_field_parent_topic ON node.nid = field_data_field_parent_topic.entity_id AND (field_data_field_parent_topic.entity_type = 'node' AND field_data_field_parent_topic.deleted = '0')
+            LEFT JOIN field_data_field_site_topics field_data_field_site_topics ON node.nid = field_data_field_site_topics.entity_id AND (field_data_field_site_topics.entity_type = 'node' AND field_data_field_site_topics.deleted = '0')
+            LEFT JOIN draggableviews_structure draggableviews_structure ON node.nid = draggableviews_structure.entity_id AND draggableviews_structure.view_name = 'draggable_subtopics' AND draggableviews_structure.view_display = 'panel_pane_1' AND draggableviews_structure.args = :nid_args
+            WHERE (( (field_data_field_parent_topic.field_parent_topic_target_id = :nid ) OR (field_data_field_site_topics.field_site_topics_target_id = :nid ) )AND(( (node.status = '1') AND (node.type IN  ('subtopic')) AND (((domain_access.realm = 'domain_id' AND domain_access.gid = 7) OR (domain_access.realm = 'domain_site' AND domain_access.gid = 0))) AND (field_data_field_parent_subtopic.field_parent_subtopic_target_id IS NULL ) AND (flagging_node.uid IS NULL ) )))
+            ORDER BY draggableviews_structure_weight_coalesce ASC, node_created DESC";
+
+    $nodes = $this->d7conn->query($sql, [
+      ':nid_args' => '["' . $topic_id . '","' . $topic_id . '"]',
+      ':nid' => $topic_id,
+    ])->fetchAll();
+
+    $results = [];
+
+    foreach ($nodes as $node) {
+      $node = $this->lookupHelper->source()->id($node->nid);
+
+      if ($node) {
+        $results[] = $node;
+      }
+    }
+
+    return $results;
+  }
+
+
+  /**
+   * Creates an entity reference link in the topic contents field
+   *
+   * @param \Drupal\node\Entity\Node $parent_node
+   *   Node to which we add the entity reference.
+   * @param int $child_nid
+   *   Entity reference node id.
+   */
+  private function createSubtopicContentRefLink($parent_node, $child_nid) {
+    // Fetch the max delta number, so we can add our new entity reference
+    // to the bottom of the list.
+    $delta = $this->dbConn->query("SELECT MAX(delta) FROM node__field_topic_content WHERE entity_id = :parent_nid", [
+      ':parent_nid' => $parent_node->id()
+    ])->fetchField(0);
+
+    // If we don't have a delta, set to -1 as we will increment in the insert
+    // call and the delta is zero indexed.
+    $delta = $delta ?? -1;
+
+    $existing_topic_data = $this->dbConn->query("SELECT
+        nftc.*
+        FROM {node__field_topic_content} nftc
+        WHERE nftc.entity_id = :entity_id
+        AND nftc.revision_id = :rev_id
+        AND nftc.field_topic_content_target_id = :target_id", [
+      ':entity_id' => $parent_node->id(),
+      ':rev_id' => $parent_node->getRevisionId(),
+      ':target_id' => $child_nid,
+    ])->fetchAll();
+
+    if (empty($existing_topic_data)) {
+      $this->dbConn->insert('node__field_topic_content')
+        ->fields([
+          'bundle' => $parent_node->bundle(),
+          'deleted' => 0,
+          'entity_id' => $parent_node->id(),
+          'revision_id' => $parent_node->getRevisionId(),
+          'langcode' => 'en',
+          'delta' => ++$delta,
+          'field_topic_content_target_id' => $child_nid,
+        ])
+        ->execute();
+    }
+  }
+
+
+
+}
