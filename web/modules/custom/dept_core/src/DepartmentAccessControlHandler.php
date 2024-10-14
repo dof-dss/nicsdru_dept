caa4a677afcb7404d880eed704f2f918448b8849
OMAHM <hello@onemanandhismouse.com>
2024-10-14 11:55:37 +0100

    DEPT-946 ~ Access for dept admins and fix wsod for administrators

--- web/modules/custom/dept_core/src/DepartmentAccessControlHandler.php
+++ web/modules/custom/dept_core/src/DepartmentAccessControlHandler.php
@@ -18,24 +18,34 @@ class DepartmentAccessControlHandler extends EntityAccessControlHandler {
   public function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
 
     switch ($operation){
+      case 'view label':
       case 'view':
         return AccessResult::allowed();
       case 'update':
         return $this->userDepartmentAccess($entity, $account);
       case 'delete':
         return AccessResult::allowedIfHasPermission($account, 'administer departments');
+      default:
+        return AccessResult::neutral();
     }
   }
 
   private function userDepartmentAccess(EntityInterface $department, AccountInterface $account) {
-    $user = \Drupal::entityTypeManager()->getStorage('user')->load($account->id());
-    $user_departments = array_column($user->get('field_domain_access')->getValue(), 'target_id');
+    $user = \Drupal::entityTypeManager()
+      ->getStorage('user')
+      ->load($account->id());
+    $user_departments = array_column($user->get('field_domain_access')
+      ->getValue(), 'target_id');
+
+    if ($user->hasPermission('administer department')) {
+      return AccessResult::allowed();
+    }
 
     if (in_array($department->id(), $user_departments)) {
       return AccessResult::allowedIfHasPermission($account, 'update department');
-    } else {
-      return AccessResult::forbidden();
     }
+
+    return AccessResult::forbidden();
   }
 
 }
