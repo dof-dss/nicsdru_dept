8930f76328354f9d2439687bdf2d3c451e5bebaa
OMAHM <hello@onemanandhismouse.com>
2024-10-14 13:34:55 +0100

    DEPT-946 ~ Update role. Comments and code standards

--- web/modules/custom/dept_core/src/DepartmentAccessControlHandler.php
+++ web/modules/custom/dept_core/src/DepartmentAccessControlHandler.php
@@ -16,20 +16,33 @@ class DepartmentAccessControlHandler extends EntityAccessControlHandler {
    * {@inheritdoc}
    */
   public function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
-
-    switch ($operation){
+    // Custom access which grants/denies permission to update Department entities
+    // based on the users permissions and account 'Domain Access' field values.
+    switch ($operation) {
       case 'view label':
       case 'view':
         return AccessResult::allowed();
+
       case 'update':
         return $this->userDepartmentAccess($entity, $account);
+
       case 'delete':
         return AccessResult::allowedIfHasPermission($account, 'administer departments');
+
       default:
         return AccessResult::neutral();
     }
   }
 
+  /**
+   * Returns the Access result based on an account permissions and Domain Access.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $department
+   *   The Department entity.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The user account.
+   *
+   */
   private function userDepartmentAccess(EntityInterface $department, AccountInterface $account) {
     $user = \Drupal::entityTypeManager()
       ->getStorage('user')
