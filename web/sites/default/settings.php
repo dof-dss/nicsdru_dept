8dc87811a25d2d2cc91030f99d507f36618e7c4c
Johan Gant <johan.gant@gmail.com>
2024-10-24 11:05:46 +0100

    Workaround for nigov dept with no -ni suffix

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -77,8 +77,18 @@ if (!empty(getenv('PLATFORM_BRANCH'))) {
       break;
 
     default:
-      $config['domain.record.finance']['hostname'] = sprintf('www.finance-ni.%s-%s.uk-1.platformsh.site', getenv('PLATFORM_ENVIRONMENT'), getenv('PLATFORM_PROJECT'));
-      $config['domain.record.communities']['hostname'] = sprintf('www.communities-ni.%s-%s.uk-1.platformsh.site', getenv('PLATFORM_ENVIRONMENT'), getenv('PLATFORM_PROJECT'));
+      // Remap domain records to match PSH environment URLs.
+      foreach (['finance', 'communities', 'health', 'infrastructure', 'daera', 'nigov', 'justice', 'economy', 'education', 'executiveoffice'] as $dept) {
+        if ($dept != 'nigov') {
+          $dept .= '-ni';
+        }
+
+        $config["domain.record.{$dept}"]['hostname'] = sprintf("www.%s.%s-%s.uk-1.platformsh.site",
+          $dept,
+          getenv('PLATFORM_ENVIRONMENT'),
+          getenv('PLATFORM_PROJECT'));
+      }
+      
       // Default to use development settings/services for general platform.sh environments.
       $config['config_split.config_split.development']['status'] = TRUE;
   }
