4b3b86f04e19c624741b464d2a6a02dfc4484386
omahm <hello@onemanandhismouse.com>
2025-01-07 15:21:47 +0000

    DEPT-1084 ~ Clear featured cache for any featured node that has a state change

--- web/modules/custom/dept_homepage/src/EventSubscriber/NodeStatusSubscriber.php
+++ web/modules/custom/dept_homepage/src/EventSubscriber/NodeStatusSubscriber.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\dept_homepage\EventSubscriber;
 
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\origins_workflow\Event\ModerationStateChangeEvent;
 use Drupal\scheduled_transitions\Event\ScheduledTransitionsEvents;
@@ -16,11 +18,26 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 final class NodeStatusSubscriber implements EventSubscriberInterface {
 
   /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected Connection $dbConn;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $database_connection
+   *   The database connection.
+   */
+  public function __construct(Connection $database_connection) {
+    $this->dbConn = $database_connection;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents(): array {
     return [
-      ModerationStateChangeEvent::CHANGE => ['onModerationStateChange'],
+      ModerationStateChangeEvent::CHANGE => ['onModerationStateChange', 100],
       ScheduledTransitionsEvents::NEW_REVISION => ['newRevision', 1011],
     ];
   }
@@ -31,7 +48,7 @@ final class NodeStatusSubscriber implements EventSubscriberInterface {
    * @param \Drupal\origins_workflow\Event\ModerationStateChangeEvent $event
    *   The Origins Workflow moderation change event.
    */
-  public function onModerationStateChange(ModerationStateChangeEvent $event) {
+  public function onModerationStateChange(ModerationStateChangeEvent $event): void {
     if ($event->isPublished() || $event->isArchived()) {
       $this->updateDepartmentFeatured($event->getEntity());
     }
@@ -43,7 +60,7 @@ final class NodeStatusSubscriber implements EventSubscriberInterface {
    * @param \Drupal\scheduled_transitions\Event\ScheduledTransitionsNewRevisionEvent $event
    *   Scheduled Transition event.
    */
-  public function newRevision(ScheduledTransitionsNewRevisionEvent $event) {
+  public function newRevision(ScheduledTransitionsNewRevisionEvent $event): void {
     $scheduledTransition = $event->getScheduledTransition();
     $state = $scheduledTransition->getState();
 
@@ -54,7 +71,24 @@ final class NodeStatusSubscriber implements EventSubscriberInterface {
 
   protected function updateDepartmentFeatured(EntityInterface $entity) {
     // Clear node domain/dept and nigov featured cache.
-    $domain = $entity->get('domain_source')->getString();
+    $domain = $entity->get('field_domain_source')->getString();
+
+    if (empty($domain)) {
+      return;
+    }
+
+    $query = $this->dbConn->select('node__field_featured_content', 'fc');
+    $query->join('node__field_domain_source', 'ds', 'fc.entity_id = ds.entity_id');
+    $query
+      ->fields('fc', ['field_featured_content_target_id'])
+      ->condition('ds.bundle', 'featured_content_list')
+      ->condition('ds.field_domain_source_target_id', $domain);
+
+    $featured_nids = $query->execute()->fetchCol();
+
+    if (in_array($entity->id(), $featured_nids)) {
+      Cache::invalidateTags(['homepage_featured:' . $domain]);
+    }
   }
 
 }
