361f68b582834824396e9e9e0778f147a3c59e5c
omahm <hello@onemanandhismouse.com>
2025-03-13 13:01:56 +0000

    DEPT-1173 ~ Update parameter type in doc comment

--- web/modules/custom/dept_topics/src/TopicManager.php
+++ web/modules/custom/dept_topics/src/TopicManager.php
@@ -314,7 +314,7 @@ final class TopicManager {
   /**
    * Returns the maximum assignable topics permitted for the given node bundle.
    *
-   * @param mixed $type
+   * @param string|ContentEntityInterface $type
    *   A node type ID or content entity.
    * @return int
    *   The maximum amount.
