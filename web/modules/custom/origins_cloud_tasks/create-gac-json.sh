8b0a9c6e32d012dcaa34f823f14a269d8af42b16
omahm <hello@onemanandhismouse.com>
2025-08-13 14:31:06 +0100

    D8O-39 ~ Run script locally

---
+++ web/modules/custom/origins_cloud_tasks/create-gac-json.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Generates GAC file from JSON data in the environment variable GOOGLE_APPLICATION_CREDENTIALS_JSON
+# See: https://cloud.google.com/docs/authentication/application-default-credentials#GAC
+
+JSON_FILE="$PLATFORM_APP_DIR/google_application_credentials.json"
+
+if [ -n "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" ]; then
+   if echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" | jq empty >/dev/null 2>&1; then
+          echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > "$JSON_FILE"
+          export GOOGLE_APPLICATION_CREDENTIALS="$JSON_FILE"
+          echo "GAC JSON file created."
+      else
+          echo "Environment variable 'GOOGLE_APPLICATION_CREDENTIALS_JSON' contains invalid JSON."
+          exit 1
+   fi
+else
+    echo "Environment variable 'GOOGLE_APPLICATION_CREDENTIALS_JSON' is not set or empty."
+    exit 1
+fi
