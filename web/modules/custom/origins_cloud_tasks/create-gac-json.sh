965c818ec2db19dc3255f53aca040545cd0206ae
omahm <hello@onemanandhismouse.com>
2025-08-13 14:47:03 +0100

    D8O-39 ~ Remove json validation

--- web/modules/custom/origins_cloud_tasks/create-gac-json.sh
+++ web/modules/custom/origins_cloud_tasks/create-gac-json.sh
@@ -6,14 +6,9 @@
 JSON_FILE="$PLATFORM_APP_DIR/google_application_credentials.json"
 
 if [ -n "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" ]; then
-   if echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" | jq empty >/dev/null 2>&1; then
-          echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > "$JSON_FILE"
-          export GOOGLE_APPLICATION_CREDENTIALS="$JSON_FILE"
-          echo "GAC JSON file created."
-      else
-          echo "Environment variable 'GOOGLE_APPLICATION_CREDENTIALS_JSON' contains invalid JSON."
-          exit 1
-   fi
+  echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > "$JSON_FILE"
+  export GOOGLE_APPLICATION_CREDENTIALS="$JSON_FILE"
+  echo "GAC JSON file created."
 else
     echo "Environment variable 'GOOGLE_APPLICATION_CREDENTIALS_JSON' is not set or empty."
     exit 1
