ae38a4ef2a7d6af778b355791ea0dd324bcb6fa7
omahm <hello@onemanandhismouse.com>
2025-09-18 10:56:19 +0100

    DEPT-1245 ~ Rename sidebar link. Remove ability to add/delete in topic content modal

--- web/modules/custom/dept_topics/src/Form/ManageTopicContentForm.php
+++ web/modules/custom/dept_topics/src/Form/ManageTopicContentForm.php
@@ -16,7 +16,7 @@ use Drupal\path_alias\AliasManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a form to allow addition, removal and sorting of topic child content.
+ * Provides a form to allow re-ordering of topic child content.
  */
 final class ManageTopicContentForm extends FormBase {
 
@@ -85,43 +85,6 @@ final class ManageTopicContentForm extends FormBase {
 
     $form['#prefix'] = '<div id="form-wrapper">';
     $form['#suffix'] = '</div>';
-
-    $form['add_existing'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Add existing content'),
-      '#attributes' => [
-        'class' => ['container-inline'],
-      ]
-    ];
-
-    // Use the Linkit profile to restrict which content can be added.
-    $form['add_existing']['add_path'] = [
-      '#type' => 'linkit',
-      '#title' => $this->t('Content URL'),
-      '#title_display' => 'invisible',
-      '#placeholder' => $this->t('Start typing a title to search...'),
-      '#description_display' => 'after',
-      '#autocomplete_route_name' => 'linkit.autocomplete',
-      '#autocomplete_route_parameters' => [
-        'linkit_profile_id' => 'topic_child_content',
-      ],
-    ];
-
-    $form['add_existing']['add'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Add'),
-      '#name' => 'add',
-      '#submit' => ['::ajaxSubmit'],
-      '#ajax' => [
-        'callback' => '::childContentCallback',
-        'wrapper' => 'form-wrapper',
-      ],
-      '#attributes' => [
-        'class' => [
-          'button--primary',
-        ],
-      ],
-    ];
 
     $form['topic_nid'] = [
       '#type' => 'hidden',
@@ -228,31 +191,6 @@ final class ManageTopicContentForm extends FormBase {
           ],
         ];
       }
-      else {
-        $form['child_content'][$child_nid]['delete'] = [
-          '#type' => 'submit',
-          '#title' => t('Remove'),
-          '#name' => 'delete_' . $child_nid,
-          '#value' => 'Remove',
-          '#submit' => ['::ajaxSubmit'],
-          '#ajax' => [
-            'callback' => '::childContentCallback',
-            'wrapper' => 'form-wrapper',
-          ],
-          '#attributes' => [
-            'class' => [
-              'button--danger',
-              'link',
-            ]
-          ],
-          '#wrapper_attributes' => [
-            'class' => [
-              'manage-topic-content-remove-cell'
-            ],
-            'title' => $this->t('Remove this content from the topic.')
-          ],
-        ];
-      }
     }
 
     $form['actions'] = ['#type' => 'actions'];
@@ -289,16 +227,6 @@ final class ManageTopicContentForm extends FormBase {
   }
 
   /**
-   * Callback to return the child content render array.
-   */
-  public function childContentCallback(array &$form, FormStateInterface $form_state) {
-    // Remove Linkit entry after adding new content.
-    $form['add_existing']['add_path']['#value'] = $form_state->getValue('add_path');
-
-    return $form;
-  }
-
-  /**
    * Ajax callback to close the modal.
    */
   public function closeModalAjax() {
@@ -307,100 +235,6 @@ final class ManageTopicContentForm extends FormBase {
     $response->addCommand($command);
 
     return $response;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    $parents = $form_state->getTriggeringElement()['#parents'];
-
-    // Append call.
-    if ($parents[0] === 'add') {
-      $add_path = $form_state->getValue('add_path');
-
-      if (empty($add_path)) {
-        $form_state->setErrorByName('add_path', 'You must provide a URL.');
-        return;
-      }
-
-      // We only want valid url paths and not the typed text.
-      if (!str_starts_with($add_path, '/node/')) {
-        $form_state->setErrorByName('add_path', 'Path must be a valid URL');
-        return;
-      }
-
-      $node_id = $this->extractNodeIdFromUrl($add_path);
-
-      $topic_manager = \Drupal::service('topic.manager');
-      $node = \Drupal::entityTypeManager()->getStorage('node')->load($node_id);
-
-      if (count($topic_manager->getParentNodes($node_id)) > TopicManager::maximumTopicsForType($node->bundle())) {
-        $form_state->setErrorByName('add_path', t('This @type has the maximum number of topics assigned and cannot be added to this content.', ['@type' => $node->bundle()]));
-      }
-
-      $child_content = $form_state->getValue('child_content');
-      $new_content_nid = $this->extractNodeIdFromUrl($add_path);
-
-      if (is_array($child_content)) {
-        // Check if there is already an entry for this node.
-        if (array_key_exists($new_content_nid, $child_content)) {
-          $form_state->setErrorByName('add_path', 'Child content entry already exists for this URL.');
-          return;
-        }
-      }
-    }
-
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function ajaxSubmit(array &$form, FormStateInterface $form_state) {
-    $parents = $form_state->getTriggeringElement()['#parents'];
-    $removed_children = $form_state->getValue('removed_children');
-
-    // Append call.
-    if ($parents[0] === 'add') {
-      $add_path = $form_state->getValue('add_path');
-
-      $child_content = empty($form_state->getValue('child_content')) ? [] : $form_state->getValue('child_content');
-      $new_content_nid = $this->extractNodeIdFromUrl($add_path);
-
-      $weight = 0;
-
-      if (is_array($child_content) && !empty($child_content)) {
-        $weight = $child_content[array_key_last($child_content)]['weight'];
-        $weight++;
-      }
-
-      $child_content[$new_content_nid] = [
-        'weight' => $weight,
-        'delete' => 'Remove',
-      ];
-
-      $form_state->setValue('child_content', $child_content);
-      // Clear the path value to remove the link in the Linkit field.
-      $form_state->setValue('add_path', '');
-    }
-
-    // Deleted call.
-    if (!empty($parents[2]) && $parents[2] === 'delete') {
-      if (!empty($removed_children)) {
-        $removed_children = explode(',', $removed_children);
-        $removed_children[] = $parents[1];
-      }
-      else {
-        $removed_children = [$parents[1]];
-      }
-
-      if (count($removed_children) > 0) {
-        $form_state->setValue('removed_children', implode(',', $removed_children));
-      }
-    }
-
-    $form_state->setRebuild(TRUE);
   }
 
   /**
@@ -433,40 +267,6 @@ final class ManageTopicContentForm extends FormBase {
    */
   public function cancel(array $form, FormStateInterface $form_state) {
     $form_state->setRedirect('entity.node.canonical', ['node' => $form_state->getValue('topic_nid')]);
-  }
-
-  /**
-   * Return the node ID for the given path.
-   */
-  protected function extractNodeIdFromUrl(string $url):int {
-    // Strip the host and match the alias to a node id.
-    if (UrlHelper::isExternal($url)) {
-      $host = $this->getRequest()->getSchemeAndHttpHost();
-      $alias = substr($url, strlen($host));
-      $path = $this->aliasManager->getPathByAlias($alias);
-      $nid = (int) substr($path, 6);
-    }
-    else {
-      // Canonical URL. Trim to extract the node id parameter.
-      $nid = (int) substr($url, 6);
-    }
-
-    return $nid;
-  }
-
-  /**
-   * @param int $nid
-   *   The node id to check.
-   *
-   * @return bool
-   *   True if the node has parents, otherwise false.
-   */
-  protected function subtopicHasParent($nid) {
-    $node = $this->entityTypeManager->getStorage('node')->load($nid);
-    if ($node && $node->bundle() === 'subtopic') {
-      return !empty($this->topicManager->getParentNodes($node));
-    }
-    return FALSE;
   }
 
 }
