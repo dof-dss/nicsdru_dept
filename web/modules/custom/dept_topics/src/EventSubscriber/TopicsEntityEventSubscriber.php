758db5971393c514a9e28123dabb76385c9bd0f7
omahm <hello@onemanandhismouse.com>
2025-10-06 15:56:29 +0100

    WiP updating topic child content via the db api

--- web/modules/custom/dept_topics/src/EventSubscriber/TopicsEntityEventSubscriber.php
+++ web/modules/custom/dept_topics/src/EventSubscriber/TopicsEntityEventSubscriber.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Drupal\dept_topics\EventSubscriber;
 
 use Drupal\book\BookManagerInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\dept_topics\OrphanManager;
 use Drupal\dept_topics\TopicContentAction;
@@ -34,82 +36,12 @@ final class TopicsEntityEventSubscriber implements EventSubscriberInterface {
   public function onEntityPresave(EntityEvent $event): void {
     $entity = $event->getEntity();
 
-    // Removed updated Topic child from the orphan list if it has site topics.
-    if ($this->topicManager->isValidTopicChild($entity) && !$entity->isNew()) {
-      $topics = $entity->get('field_site_topics')->getValue();
-      if (count($topics) > 0) {
-        $this->orphanManager->removeOrphan($entity);
-      }
+    if (!$this->topicManager->isValidTopicChild($entity)) {
+      return;
     }
 
-    if ($entity->bundle() === 'topic' || $entity->bundle() === 'subtopic') {
-      $moderation_state = $entity->get('moderation_state')->getString();
 
-      if ($moderation_state === 'published' || $moderation_state === 'archived') {
 
-        // Remove any orphaned content that is assigned to a new published topic.
-        if ($entity->isNew() && $moderation_state === 'published') {
-          $topic_content = array_column($entity->get('field_topic_content')->getValue(), 'target_id');
-          $this->orphanManager->processTopicContents($topic_content);
-          return;
-        }
-
-        // Add or remove site topic tags to nodes that are added or removed from topic child contents.
-        // @phpstan-ignore-next-line
-        $original = array_column($entity->original->get('field_topic_content')->getValue(), 'target_id');
-        // @phpstan-ignore-next-line
-        $updated = array_column($entity->get('field_topic_content')->getValue(), 'target_id');
-
-        $removed = array_diff($original, $updated);
-        $added = array_diff($updated, $original);
-
-        if ($removed) {
-          foreach ($removed as $nid) {
-
-            // Do not remove site topics from the node if it is a book page.
-            if ($this->bookManager->loadBookLink($nid) === TRUE) {
-              continue;
-            }
-
-            $child_node = $this->entityTypeManager->getStorage('node')->load($nid);
-
-            if (!empty($child_node)) {
-              $child_topics = $child_node->get('field_site_topics');
-
-              for ($i = 0; $i < $child_topics->count(); $i++) {
-                // @phpstan-ignore-next-line
-                if ($child_topics->get($i)->target_id == $entity->id()) {
-                  $child_topics->removeItem($i);
-                  $i--;
-                }
-              }
-              $child_node->save();
-
-              if ($child_topics->count() == 0) {
-                $this->orphanManager->addOrphan($child_node, $entity);
-              }
-            }
-          }
-        }
-
-        if ($added) {
-          foreach ($added as $nid) {
-            $child_node = $this->entityTypeManager->getStorage('node')->load($nid);
-            if (!empty($child_node)) {
-              $child_topic_tags = array_column($child_node->get('field_site_topics')->getValue(), 'target_id');
-
-              if (!in_array($entity->id(), $child_topic_tags)) {
-                $child_node->get('field_site_topics')->appendItem([
-                  'target_id' => $entity->id()
-                ]);
-                $child_node->save();
-                $this->orphanManager->removeOrphan($child_node);
-              }
-            }
-          }
-        }
-      }
-    }
 
   }
 
@@ -117,49 +49,72 @@ final class TopicsEntityEventSubscriber implements EventSubscriberInterface {
    * Entity insert event handler.
    */
   public function onEntityInsert(EntityEvent $event): void {
+    /* @var ContentEntityInterface $child */
+    $child = $event->getEntity();
+    if (!$this->topicManager->isValidTopicChild($child)) {
+      return;
+    }
+
+    if ($child->get('moderation_state')->getString() == 'published') {
+      $topics = $child->get('field_site_topics')->referencedEntities();
+
+      foreach ($topics as $topic) {
+        $this->topicManager->addChildToTopic($child, $topic);
+      }
+    }
   }
 
   /**
    * Entity update event handler.
    */
   public function onEntityUpdate(EntityEvent $event): void {
+    $child = $event->getEntity();
+
+    if (!$this->topicManager->isValidTopicChild($child)) {
+      return;
+    }
+
+    $original = $child->original;
+
+    if (!$child->get('moderation_state')->getString() == 'published') {
+    }
+
+    $existing_topics = array_column($original->get('field_site_topics')->getValue(), 'target_id');
+    $updated_topics = array_column($child->get('field_site_topics')->getValue(), 'target_id');
+
+    $topics_added_ids = array_diff($updated_topics, $existing_topics);
+    $topics_removed_ids = array_diff($existing_topics, $updated_topics);
+
+    if (!empty($topics_removed_ids) || !empty($topics_added_ids)) {
+      $node_store = $topic = $this->entityTypeManager->getStorage('node');
+    }
+
+    foreach ($topics_added_ids as $topic_id) {
+      $topic = $node_store->load($topic_id);
+      $this->topicManager->addChildToTopic($child, $topic);
+    }
+
+    foreach ($topics_removed_ids as $topic_id) {
+      $topic = $node_store->load($topic_id);
+      $this->topicManager->removeChildFromTopic($child, $topic);
+    }
+
+
   }
 
   /**
    * Entity delete event handler.
    */
   public function onEntityDelete(EntityEvent $event): void {
-    $entity = $event->getEntity();
-
-    // Cleanup in orphan data for this node.
-    if (in_array($entity->bundle(), $this->topicManager->getTopicChildNodeTypes())) {
-      $this->orphanManager->removeOrphan($entity);
+    $child = $event->getEntity();
+    if (!$this->topicManager->isValidTopicChild($child)) {
+      return;
     }
 
-    if ($entity->bundle() === 'topic' || $entity->bundle() === 'subtopic') {
-      // Process orphaned.
-      $child_contents = array_column($entity->get('field_topic_content')->getValue(), 'target_id');
+    $topics = $child->get('field_site_topics')->referencedEntities();
 
-      foreach ($child_contents as $child_id) {
-        $child_node = $this->entityTypeManager->getStorage('node')->load($child_id);
-        $child_topics = $child_node->get('field_site_topics');
-
-        for ($i = 0; $i < $child_topics->count(); $i++) {
-          // @phpstan-ignore-next-line
-          if ($child_topics->get($i)->target_id == $entity->id()) {
-            $child_topics->removeItem($i);
-            $i--;
-          }
-        }
-        $child_node->save();
-
-        if ($child_topics->count() == 0) {
-          $this->orphanManager->addOrphan($child_node, $entity);
-        }
-      }
-
-      // Cleanup in orphan data for this node.
-      $this->orphanManager->removeOrphan($entity);
+    foreach ($topics as $topic) {
+      $this->topicManager->removeChildFromTopic($child, $topic);
     }
   }
 
