d2235bb918059aa26c54e7f25e530ef745f42a83
omahm <hello@onemanandhismouse.com>
2026-05-12 13:51:23 +0100

    DEPT-1365 ~ Update entity field target ids

--- web/modules/custom/dept_fs/src/Form/MediaConsolidatorConfirmForm.php
+++ web/modules/custom/dept_fs/src/Form/MediaConsolidatorConfirmForm.php
@@ -45,14 +45,20 @@ class MediaConsolidatorConfirmForm extends ConfirmFormBase {
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $store = $this->tempStoreFactory->get('media_consolidator');
     $mids = $store->get('selected_media') ?? [];
+//    $store->delete('selected_media');
 
     /** @var \Drupal\media\MediaInterface[] $entities */
     $entities = $this->entityTypeManager->getStorage('media')->loadMultiple($mids);
 
     $form['#title'] = $this->t('Consolidate Media');
+    $form['mids'] = [
+      '#type' => 'hidden',
+      '#value' => implode(',', $mids),
+    ];
 
     $options = [];
     foreach ($entities as $media) {
+
       $options[$media->id()] = $this->t(
         '@name (@bundle, ID: @id)',
         [
@@ -63,7 +69,7 @@ class MediaConsolidatorConfirmForm extends ConfirmFormBase {
       );
     }
 
-    $form['media_origin'] = [
+    $form['media_original'] = [
       '#type' => 'radios',
       '#title' => $this->t('Select the media origin, this will replace all of the duplicates on this page.'),
       '#options' => $options,
@@ -95,7 +101,67 @@ class MediaConsolidatorConfirmForm extends ConfirmFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $media_storage = \Drupal::entityTypeManager()->getStorage('media');
+    $database = \Drupal::database();
+    $original_mid = $form_state->getValue('media_original');
+    $mids = explode(',', $form_state->getValue('mids'));
+    $mids = array_diff($mids, [$original_mid]);
+    $entities = $media_storage->loadMultiple($mids);
+    $entity_usage = \Drupal::service('entity_usage.usage');
+    $sources = [];
 
+    foreach ($entities as $entity) {
+      $usage = $entity_usage->listSources($entity);
+
+      foreach ($usage as $entity_type => $ids) {
+        if (!array_key_exists($entity_type, $sources)) {
+          $sources[$entity_type] = [];
+        }
+
+        foreach ($ids as $id => $items) {
+          foreach ($items as $item) {
+            $field_name = $item['field_name'];
+            if (!array_key_exists($field_name, $sources[$entity_type])) {
+              $sources[$entity_type][$field_name] = [];
+            }
+
+            if (!in_array($entity->id(), $sources[$entity_type][$field_name])) {
+              array_push($sources[$entity_type][$field_name], $entity->id());
+            }
+
+          }
+        }
+      }
+
+      foreach($sources as $entity_type => $fields) {
+        foreach ($fields as $field => $data) {
+          $table = $entity_type . "__" . $field;
+          $target_column = $field . "_target_id";
+
+          $database->update($table)
+            ->fields([$target_column => $original_mid])
+            ->condition($target_column, $mids, 'IN')
+            ->execute();
+
+          // Update revisions
+          $table = $entity_type . "_revision__" . $field;
+
+          $database->update($table)
+            ->fields([$target_column => $original_mid])
+            ->condition($target_column, $mids, 'IN')
+            ->execute();
+
+        }
+      }
+
+
+
+      // TODO: update target for usage place
+      // TODO: cleaup entity usage records by calling bulkDeleteTargets()
+
+    }
+
+    ksm($sources);
   }
 
   /**
