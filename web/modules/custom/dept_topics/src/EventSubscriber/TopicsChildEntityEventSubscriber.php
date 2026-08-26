dd78db6bcc47b73e1b8618f87ae6793871191bdd
omahm <hello@onemanandhismouse.com>
2026-08-26 13:17:58 +0100

    DEPT-1249 ~ Prevent drafts from updating topics if the node is published and warn user

--- web/modules/custom/dept_topics/src/EventSubscriber/TopicsChildEntityEventSubscriber.php
+++ web/modules/custom/dept_topics/src/EventSubscriber/TopicsChildEntityEventSubscriber.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\dept_topics\EventSubscriber;
 
+use Drupal\content_moderation\ModerationInformationInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\dept_topics\TopicManager;
 use Drupal\entity_events\EntityEventType;
 use Drupal\entity_events\Event\EntityEvent;
@@ -20,6 +23,9 @@ final class TopicsChildEntityEventSubscriber implements EventSubscriberInterface
    */
   public function __construct(
     private readonly TopicManager $topicManager,
+    private readonly ModerationInformationInterface $moderationInformation,
+    private readonly MessengerInterface $messenger,
+    private readonly EntityTypeManagerInterface $entityTypeManager,
   ) {}
 
   /**
@@ -52,6 +58,7 @@ final class TopicsChildEntityEventSubscriber implements EventSubscriberInterface
       return;
     }
 
+    $is_published = $this->moderationInformation->isDefaultRevisionPublished($entity);
     $moderation_state = $entity->get('moderation_state')->getString();
 
     switch ($moderation_state) {
@@ -59,6 +66,24 @@ final class TopicsChildEntityEventSubscriber implements EventSubscriberInterface
         $this->topicManager->archiveChild($entity);
         break;
 
+      case 'draft':
+      case 'needs_review':
+        if ($is_published) {
+          $published_entity = $this->entityTypeManager->getStorage($entity->getEntityTypeId())->load($entity->id());
+
+          $current_topics = array_column($entity->get('field_site_topics')->getValue(), 'target_id');
+          $published_topics = array_column($published_entity->get('field_site_topics')->getValue(), 'target_id');
+          sort($current_topics);
+          sort($published_topics);
+
+          if ($current_topics !== $published_topics) {
+            $this->messenger->addMessage("This content already has a published revision, and the Topics you've selected differ from that published version. The new Topics will not take effect until this revision is published.");
+          }
+        }
+        else {
+          $this->topicManager->processChild($entity);
+        }
+        break;
       default:
         $this->topicManager->processChild($entity);
         break;
